c3aa72856add78873442c38195e729d83567b4cf
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-03-19 12:50:25 +0000

    Boilerplate Server Layout and Module Docker Dev

---
+++ src/modules/bmd-videohub/server/server.js
@@ -0,0 +1,12 @@
+//NAME: app.js
+//AUTH: Ryan McCartney <ryan.mccartney@bbc.co.uk>
+//DATE: 19/03/2021
+//DESC: Blackmagic Design VideoHub Module
+
+//server.js
+const app = require("./app");
+let port = process.env.MODULE_PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Example app listening on port "+port.toString());
+});
