c96c2d54f51af1264825e92686e42812c94cce68
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-03-23 18:07:10 +0000

    Docker functions moved to services

--- src/services/panel-start.js
+++ src/services/panel-start.js
@@ -7,7 +7,19 @@ const config = require('@services/panel-getconfig');
 
 module.exports = async (panelId) => {
     try {
-       return docker.startContainer(await config(panelId))
+
+        //STEP 1 - Build the image for the module, if it's already been done this will be quick
+        const image = buildImage(panelConfig.module)
+    
+        //STEP 2 - Check if the container already exists, if not create one
+    
+        //STEP 3 - Launch the Container
+    
+    
+        //STEP 4 - Pass the config
+        state.container = docker.getContainer(panelConfig.id);        
+
+       return
     } catch (error) {
         logger.warn(`panel-start: ${error.trace || error || error.message}`);
     }
