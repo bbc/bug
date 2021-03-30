7da1842c29b05449dc6d4e20bdb4702693b93192
Geoff House <geoff.house@bbc.co.uk>
2021-03-30 13:21:44 +0100

    add notistack

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -4,15 +4,30 @@ import Page from "@pages/Page";
 import theme from "./theme";
 import PanelList from "@data/PanelList";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { SnackbarProvider } from "notistack";
+import Fade from '@material-ui/core/Fade';
 
 export default function App() {
     return (
         <>
             <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <PanelList>
-                    <Page></Page>
-                </PanelList>
+                <SnackbarProvider
+                    hideIconVariant
+                    dense
+                    autoHideDuration="3000"
+                    preventDuplicate
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "right",
+                    }}
+                    TransitionComponent={Fade}
+                    maxSnack={3}
+                >
+                    <CssBaseline />
+                    <PanelList>
+                        <Page></Page>
+                    </PanelList>
+                </SnackbarProvider>
             </ThemeProvider>
         </>
     );
