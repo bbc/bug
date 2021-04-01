c0b8265de21cdba7264bc4f26735dbfe4f14e090
Geoff House <geoff.house@bbc.co.uk>
2021-04-01 11:47:09 +0100

    improve dynamic panel load

--- src/client/src/utils/AxiosGet.js
+++ src/client/src/utils/AxiosGet.js
@@ -4,14 +4,14 @@ export default async function AxiosGet(url) {
 
     const response = await axios.get(url);
 
-    switch(response.status) {
+    switch(response.data.status) {
         case 'success':
-            return response.data;
+            return response.data.data;
         case 'fail':
             console.error('failed to fetch ' + url);
             return null;
         case 'error':
-            console.error(response.message);
+            console.error(response.data.message);
             return null;
         default:
             return null;
