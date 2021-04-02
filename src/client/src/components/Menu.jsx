cd8e7ecc80e220c505d57276290a1cea271cf95d
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-04-02 11:45:51 +0100

    Docker Compose Fragments

--- src/client/src/components/Menu.jsx
+++ src/client/src/components/Menu.jsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBug } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+
 import DynamicIcon from "@utils/DynamicIcon";
 import Loading from "./Loading";
 import { PanelContext } from "@data/PanelList";
