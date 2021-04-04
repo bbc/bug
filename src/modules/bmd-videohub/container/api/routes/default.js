12cacb7e46f9a5cdb599612ce3db500960dcf788
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-04-04 10:28:41 +0100

    Trying to get tests to pass

--- src/modules/bmd-videohub/container/api/routes/default.js
+++ src/modules/bmd-videohub/container/api/routes/default.js
@@ -6,50 +6,8 @@
 var express = require('express'),
 defaultRoute = express.Router();
 
-defaultRoute.get('/', function(req, res){
+defaultRoute.use('/', function(req, res){
     
-  var completeURL = req.protocol + '://' + req.get('host') + req.originalUrl;
-  let status = {
-    request_url: completeURL,
-    request_method: req.method,
-    request_params: req.query,
-    error: "Invalid API route, check the API documentation."
-  }
-
-  res.header("Content-Type",'application/json');
-  res.json(status);
-  })
-
-defaultRoute.post('/', function(req, res){
-  
-  var completeURL = req.protocol + '://' + req.get('host') + req.originalUrl;
-  let status = {
-    request_url: completeURL,
-    request_method: req.method,
-    request_params: req.query,
-    error: "Invalid API route, check the API documentation."
-  }
-
-  res.header("Content-Type",'application/json');
-  res.json(status);
-  })
-
-defaultRoute.delete('/', function(req, res){
-
-  var completeURL = req.protocol + '://' + req.get('host') + req.originalUrl;
-  let status = {
-    request_url: completeURL,
-    request_method: req.method,
-    request_params: req.query,
-    error: "Invalid API route, check the API documentation."
-  }
-
-  res.header("Content-Type",'application/json');
-  res.json(status);
-  })
-
-defaultRoute.put('/', function(req, res){
-
   var completeURL = req.protocol + '://' + req.get('host') + req.originalUrl;
   let status = {
     request_url: completeURL,
