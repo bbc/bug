35f7257a20e31fe00b088a473e48b559ccbb309f
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-04-04 19:36:19 +0100

    Sample Client Test

--- src/client/src/App.test.jsx
+++ src/client/src/App.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from 'components/App';
+import App from './App';
 
-test('renders learn react link', () => {
+test('Renders BUG home page.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to BUG/i);
   expect(linkElement).toBeInTheDocument();
 });
