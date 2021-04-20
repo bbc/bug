667ad4d90cc66c25cdeb60c2c64e0915cf3f6060
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-04-20 17:39:20 +0100

    Get default config when adding a container

--- src/services/panel-add.js
+++ src/services/panel-add.js
@@ -3,16 +3,22 @@
 const path = require('path');
 const id = require('@utils/id');
 const setConfig = require('@services/panel-configset');
+const moduleGet = require('@services/module-get');
 const logger = require('@utils/logger');
 
 //TODO - Are we taking the right approach here? Is there other things we want to do when a panel is added?
-
 module.exports = async (panelConfig) => {
     try {
+        //Get an ID if one hasn't been passd
         if(panelConfig.id === undefined){
-            panelConfig.id = await id()
+            panelConfig.id = await id();
         }
-        const status = await setConfig(panelConfig)
+
+        //Merge config passed with default module config
+        const module = await moduleGet(panelConfig.module);
+        panelConfig = { ...module?.defaultconfig, ...panelConfig };
+
+        const status = await setConfig(panelConfig);
         return status;
 
     } catch (error) {
