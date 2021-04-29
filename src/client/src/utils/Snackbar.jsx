9361244d4a945fa344e9c0ebf125e64cc2ff7d87
Geoff House <geoff.house@bbc.co.uk>
2021-04-29 20:03:22 +0100

    tidy

--- src/client/src/utils/Snackbar.jsx
+++ src/client/src/utils/Snackbar.jsx
@@ -1,68 +1,65 @@
-import { useSnackbar } from 'notistack';
-import React, { useEffect } from 'react';
+import { useSnackbar } from "notistack";
+import React, { useEffect } from "react";
 import Socket from "@utils/Socket";
 
 const socket = Socket();
 
 const InnerSnackbarConfigurator = (props) => {
-  props.setUseSnackbarRef(useSnackbar())
-  return null;
-}
+    props.setUseSnackbarRef(useSnackbar());
+    return null;
+};
 
 let useSnackbarRef;
 const setUseSnackbarRef = (useSnackbarRefProp) => {
-  useSnackbarRef = useSnackbarRefProp
-}
+    useSnackbarRef = useSnackbarRefProp;
+};
 
 export function SnackbarConfigurator() {
+    const { enqueueSnackbar } = useSnackbar();
 
-  const { enqueueSnackbar } = useSnackbar();
+    useEffect(() => {
+        socket.on("alert", (payload) => {
+            enqueueSnackbar(payload?.message, payload?.options);
+        });
+    });
 
-  useEffect(() => {
-    socket.on("alert",(payload) => {    
-      enqueueSnackbar(payload?.message,payload?.options);
-    });  
-  });
-
-  return(
-    <>
-      <InnerSnackbarConfigurator setUseSnackbarRef={setUseSnackbarRef} />
-    </>
-  );
+    return (
+        <>
+            <InnerSnackbarConfigurator setUseSnackbarRef={setUseSnackbarRef} />
+        </>
+    );
 }
 
 export const useAlert = () => {
+    const { enqueueSnackbar } = useSnackbar();
 
-  const { enqueueSnackbar } = useSnackbar();
+    const sendAlert = (message, options) => {
+        enqueueSnackbar(message, options);
+        if (options?.broadcast) {
+            socket.emit("alert", {
+                message: message,
+                options: options,
+            });
+        }
+    };
 
-  const sendAlert = (message,options) => {
-    enqueueSnackbar(message,options);
-    if(options?.broadcast){
-      socket.emit('alert',{
-        message: message,
-        options: options
-      });
-    }
-  }
-
-  return sendAlert
-}
-
+    return sendAlert;
+};
 
 export const snackActions = {
-  success(msg) {
-    this.toast(msg, 'success')
-  },
-  warning(msg) {
-    this.toast(msg, 'warning')
-  },
-  info(msg) {
-    this.toast(msg, 'info')
-  },
-  error(msg) {
-    this.toast(msg, 'error')
-  },
-  toast(msg, variant = 'default') {
-    useSnackbarRef.enqueueSnackbar(msg, { variant })
-  }
-}
+    success(msg) {
+        this.toast(msg, "success");
+    },
+    warning(msg) {
+        this.toast(msg, "warning");
+    },
+    info(msg) {
+        this.toast(msg, "info");
+    },
+    error(msg) {
+        this.toast(msg, "error");
+    },
+    toast(msg, variant = "default") {
+        useSnackbarRef.enqueueSnackbar(msg, { variant });
+    },
+};
