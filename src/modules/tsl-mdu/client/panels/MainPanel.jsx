e15c996d12be2fd7abfb53d9f20043647a05782c
Geoff House <geoff.house@bbc.co.uk>
2021-05-05 17:24:48 +0100

    lint

--- src/modules/tsl-mdu/client/panels/MainPanel.jsx
+++ src/modules/tsl-mdu/client/panels/MainPanel.jsx
@@ -2,55 +2,49 @@ import React, { useState, useEffect, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 import AxiosGet from "@utils/AxiosGet";
-import Grid from '@material-ui/core/Grid';
-import PanelContext from '@core/PanelContext';
+import Grid from "@material-ui/core/Grid";
+import PanelContext from "@core/PanelContext";
 
-import OutputCard from '../components/OutputCard';
+import OutputCard from "../components/OutputCard";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  }
+    root: {
+        flexGrow: 1,
+    },
 }));
 
 export default function MainPanel() {
+    const config = useContext(PanelContext);
 
-  const config = useContext(PanelContext);
+    const classes = useStyles();
+    const [outputs, setOutputs] = useState([]);
+    const [status, setStatus] = useState(null);
 
-  const classes = useStyles();
-  const [outputs, setOutputs] = useState([]);
-  const [status, setStatus] = useState(null);
+    const getStatus = async () => {
+        const response = await AxiosGet(`/container/${config?.id}/status`);
+        setStatus(response?.mdu?.status);
+        setOutputs(response?.mdu?.outputs);
+    };
 
-  const getStatus = async () => {
-    const response = await AxiosGet(`/container/${config?.id}/status`);
-    setStatus(response?.mdu?.status);
-    setOutputs(response?.mdu?.outputs);
-  }
+    useEffect(() => {
+        getStatus();
+        const interval = setInterval(getStatus, 3000);
+        return () => clearInterval(interval);
+    }, []);
 
-  useEffect(() => {
-    getStatus();
-    const interval = setInterval(getStatus, 3000);
-    return () => clearInterval(interval);
-  }, []);
+    const renderOutputs = () => {
+        const ouputs = [];
+        for (let output of outputs) {
+            ouputs.push(<OutputCard {...output} />);
+        }
+        return outputs;
+    };
 
-  const renderOutputs = () => {
-    const ouputs = [];
-    for (let output of outputs) {
-      ouputs.push(<OutputCard {...output} />);
-    }
-    return outputs
-  }
-
-  return (
-    <div className={classes.root}>
-      <Grid
-        container spacing={4}
-        direction="column"
-        justify="center"
-        alignItems="center"
-      >
-        {renderOutputs()}
-      </Grid>
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={4} direction="column" justify="center" alignItems="center">
+                {renderOutputs()}
+            </Grid>
+        </div>
+    );
 }
