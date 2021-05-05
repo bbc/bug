4a6c74c7b77a081f9f2d5b495e891fb441e7759c
Geoff House <geoff.house@bbc.co.uk>
2021-05-05 08:25:35 +0100

    improve panel operations

--- src/client/src/utils/AxiosCommand.js
+++ src/client/src/utils/AxiosCommand.js
@@ -1,31 +1,23 @@
-import axios from 'axios';
-import { snackActions } from '@utils/Snackbar';
+import axios from "axios";
 
-export default async function AxiosComand(url) {
+export default async function AxiosCommand(url) {
+    try {
+        const response = await axios.get(url);
 
-    const response = await axios.get(url);
-
-    const showMessage = (response) => {
-        if(!response.message) {
-            return;
+        switch (response.data?.status) {
+            case "success":
+                return true;
+            case "fail":
+                console.error("failed to fetch " + url);
+                return false;
+            case "error":
+                console.error(response.message);
+                return false;
+            default:
+                return false;
         }
-
-        snackActions.toast(response.message, response.status);
-
-    };
-
-    showMessage(response.data);
-
-    switch(response.status) {
-        case 'success':
-            return true;
-        case 'fail':
-            console.error('failed to fetch ' + url);
-            return false;
-        case 'error':
-            console.error(response.message);
-            return false;
-        default:
-            return false;
+    } catch (error) {
+        console.log("ERROR", error);
+        return false;
     }
 }
