453f0d582108166571a7f408d54be74b39160003
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-05-07 18:40:08 +0100

    Mongo Class Cleanup

--- src/modules/mikrotik-interfaces/container/utils/mongo-db.js
+++ src/modules/mikrotik-interfaces/container/utils/mongo-db.js
@@ -2,22 +2,19 @@ const { MongoClient } = require("mongodb");
 const url = `mongodb://bug-mongo:27017`;
 
 class Mongo {
-    constructor(dbName) {
+    constructor() {
         this.client = new MongoClient(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            serializeFunctions: true,
         });
     }
 
     async connect(dbName) {
-        if (this.client.isConnected()) {
-            // already connected
-            return;
+        if (!this.client.isConnected()) {
+            console.log(`mongo-db: connecting to mongo db at ${url}`);
+            await this.client.connect();
+            console.log("mongo-db: connected to mongo db OK");
         }
-        console.log(`mongo-db: connecting to mongo db at ${url}`);
-        await this.client.connect();
-        console.log("mongo-db: connected to mongo db OK");
         console.log(`mongo-db: opening database ${dbName}`);
         this.db = this.client.db(dbName);
     }
