1f91a094468ba89f3a840324194a7c3e7faea937
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-05-07 18:40:29 +0100

    worker instance cleanup

--- src/modules/mikrotik-interfaces/container/api/workers.js
+++ src/modules/mikrotik-interfaces/container/api/workers.js
@@ -1,14 +1,11 @@
 const WorkerManager = require('../utils/WorkerManager');
 const configGet = require("../services/config-get");
 
-let workerManager;
-
-const instantiate = async () => {
-    const config = await configGet();
-    workerManager = await new WorkerManager({ config: config })
+const instantiate = () => {
+    const config = configGet();
+    return new WorkerManager({ config: config });
 }
 
-instantiate();
+const manager = instantiate();
 
-module.exports = workerManager;
-
+module.exports = manager;
