ea431b2094745bd435a7b15c552c1c6cda80325b
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-05-10 10:57:43 +0100

    PanelList uses Socket.js

--- src/client/src/data/PanelList.jsx
+++ src/client/src/data/PanelList.jsx
@@ -1,19 +1,17 @@
 import React, { useEffect } from 'react';
-import socketIOClient from "socket.io-client";
 import { useDispatch } from 'react-redux';
 import panelListSlice from '../redux/panelListSlice';
+import Socket from "@utils/Socket";
 
 export const PanelContext = React.createContext();
 
+const socket = Socket();
+
 export default function PanelList(props) {
 
-    //TODO can we use the proxy for this?
-    const socketHost = 'http://localhost:3101'
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const socket = socketIOClient(socketHost);
-
         socket.on("connect", () => {
             socket.emit('panel');
         });
--- src/client/src/utils/Socket.js
+++ src/client/src/utils/Socket.js
@@ -1,7 +1,6 @@
 import socketIOClient from "socket.io-client";
 
 export default function Socket() {
-
     //TODO can we use the proxy for this?
     const socketHost = 'http://localhost:3101'
     const socket = socketIOClient(socketHost);
