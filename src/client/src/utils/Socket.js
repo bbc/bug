7328410837528ff1e4bd33ba564d9587155b8c15
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-05-10 11:22:21 +0100

    Working WS Proxy

--- src/client/src/utils/Socket.js
+++ src/client/src/utils/Socket.js
@@ -2,8 +2,8 @@ import socketIOClient from "socket.io-client";
 
 export default function Socket() {
     //TODO can we use the proxy for this?
-    const host = window.location.host.split(':')
-    const socketHost = `http://${host[0]}:3101`
+    const host = window.location.host;
+    const socketHost = `http://${host}`
     const socket = socketIOClient(socketHost);
     return socket
 }
