854c909270e729e49853b9a9fc6ed81696dc28a4
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-05-10 11:29:30 +0100

    Socket.js Cleanup

--- src/client/src/utils/Socket.js
+++ src/client/src/utils/Socket.js
@@ -1,9 +1,6 @@
 import socketIOClient from "socket.io-client";
 
 export default function Socket() {
-    //TODO can we use the proxy for this?
-    const host = window.location.host;
-    const socketHost = `http://${host}`
-    const socket = socketIOClient(socketHost);
+    const socket = socketIOClient();
     return socket
 }
