df7d006b97a435b3e95520755e9caea227fcfda8
Geoff House <geoff.house@bbc.co.uk>
2021-05-13 07:41:40 +0100

    fix cancel on api poller

--- src/client/src/utils/ApiPoller.jsx
+++ src/client/src/utils/ApiPoller.jsx
@@ -1,5 +1,4 @@
-import { useRef, useState } from "react";
-import useAsyncEffect from "use-async-effect";
+import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 
 export function useApiPoller({ url, interval }) {
@@ -10,75 +9,79 @@ export function useApiPoller({ url, interval }) {
         error: null,
     });
     const localResult = useRef(pollResult);
-    const cancelToken = useRef(axios.CancelToken.source());
 
-    useAsyncEffect(
-        async () => {
-            const triggerUpdate = (newState) => {
-                // this method checks if the data has changed
-                if (JSON.stringify(localResult.current) !== JSON.stringify(newState)) {
-                    // it has - store the local value
-                    localResult.current = newState;
+    useEffect(() => {
+        
+        const source = axios.CancelToken.source();
+        const cancelToken = source.token;
 
-                    // and update the state (to trigger a render in the parent component)
-                    setPollResult(newState);
+        console.log("mount", url);
+        const triggerUpdate = (newState) => {
+            // this method checks if the data has changed
+            if (JSON.stringify(localResult.current) !== JSON.stringify(newState)) {
+                // it has - store the local value
+                localResult.current = newState;
+
+                // and update the state (to trigger a render in the parent component)
+                setPollResult(newState);
+            }
+        };
+
+        const fetch = async () => {
+            console.log("fetch", url);
+            // clear any pending refresh
+            clearTimeout(timer.current);
+
+            try {
+                // fetch the data from the API
+                const response = await axios.get(url, {
+                    cancelToken: cancelToken,
+                });
+
+                // if we get an error from the API, throw it as an exception
+                if (response.data.status === "error") {
+                    throw response.data.message;
                 }
-            };
 
-            const fetch = async () => {
-                // clear any pending refresh
-                clearTimeout(timer.current);
+                triggerUpdate({
+                    status: "success",
+                    data: response.data.data,
+                    error: null,
+                });
 
-                try {
-                    // fetch the data from the API
-                    const response = await axios.get(url, {
-                        cancelToken: cancelToken.current.token,
-                    });
-
-                    // if we get an error from the API, throw it as an exception
-                    if (response.data.status === "error") {
-                        throw response.data.message;
-                    }
-
-                    triggerUpdate({
-                        status: "success",
-                        data: response.data.data,
-                        error: null,
-                    });
-
-                    // now we're done, set a timeout for the next poll
-                    timer.current = setTimeout(fetch, interval);
-                } catch (error) {
-                    // if we've cancelled the axios request (by unloading this component) just quit - no problem!
-                    if (axios.isCancel(error)) {
-                        return;
-                    }
-
-                    // send an update with the failed state
-                    setPollResult({
-                        status: "failed",
-                        data: null,
-                        error: null,
-                    });
-
-                    // log the error for good measure
-                    console.error(error);
-
-                    // and start it all again!
-                    timer.current = setTimeout(fetch, interval);
+                // now we're done, set a timeout for the next poll
+                timer.current = setTimeout(fetch, interval);
+            } catch (error) {
+                // if we've cancelled the axios request (by unloading this component) just quit - no problem!
+                if (axios.isCancel(error)) {
+                    return;
                 }
-            };
-            fetch();
-        },
-        () => {
+
+                // send an update with the failed state
+                setPollResult({
+                    status: "failed",
+                    data: null,
+                    error: null,
+                });
+
+                // log the error for good measure
+                console.error(error);
+
+                // and start it all again!
+                timer.current = setTimeout(fetch, interval);
+            }
+        };
+        fetch();
+        return () => {
+            console.log("cancel");
             // this is run when the component is unloaded
             // first of all - clear any pending requests
             clearTimeout(timer.current);
+            console.log(cancelToken);
 
             // cancel any in-flight axios requests
-            cancelToken.current.cancel();
-        },
-        [url, interval]
-    );
+            source.cancel();
+        };
+    }, [url, interval]);
     return pollResult;
 }
