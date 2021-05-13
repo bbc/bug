47e5055e52fb065292ae82aedc3c826c3db436a8
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-05-13 17:19:52 +0100

    Updated production

--- src/bin/bug-core-prod.js
+++ src/bin/bug-core-prod.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 const register = require('module-alias/register')
-const express = require('express');
-const path = require('path');
-const logger = require('@utils/logger')(module);
-
-// import the API logic
 const bugApi = require('@bin/api');
+const bugSocket = require('@bin/socket');
+const bugWorkers = require('@bin/workers');
+const logger = require('@utils/logger')(module);
+const http = require('http');
 
-// get the port from the .env file
-const port = process.env.BUG_CORE_PORT || '3100';
+const port = process.env.BUG_CORE_PORT || '80';
+
+bugApi.set('port', port);
 
 // include react static client files
 bugApi.use(express.static(path.join(__dirname, '..','client', 'build')));
@@ -19,5 +19,45 @@ bugApi.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
-// and start it up
-bugApi.listen(port, () => logger.info(`bug listening on port ${port}`));
+const server = http.createServer(bugApi);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+//Give the server to sockets as well
+bugSocket(server);
+
+// and load the worker thread
+bugWorkers();
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            logger.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            logger.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    logger.info(`bug listening on ${bind}`);
+}
