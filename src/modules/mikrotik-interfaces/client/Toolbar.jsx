69fbc7efe697319a02c6ab73e6a08316e5d05224
Geoff House <geoff.house@bbc.co.uk>
2021-05-13 08:29:18 +0100

    remove dummy toolbar items

--- src/modules/mikrotik-interfaces/client/Toolbar.jsx
+++ src/modules/mikrotik-interfaces/client/Toolbar.jsx
@@ -1,33 +1,19 @@
 import React from "react";
 import PanelToolbar from "@core/PanelToolbar";
-import Button from "@material-ui/core/Button";
-import MenuItem from "@material-ui/core/MenuItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
-import AlbumIcon from '@material-ui/icons/Album';
+// import Button from "@material-ui/core/Button";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import ListItemText from "@material-ui/core/ListItemText";
 
 export default function Toolbar(props) {
+    let toolbarProps = { ...props };
 
-    let toolbarProps = {...props};
+    const buttons = () => null;
 
-    const buttons = () => (
-        <Button variant="outlined" color="primary" startIcon={<AirplanemodeActiveIcon />} onClick={() => { alert("TAKEOFF!!!") }}>
-            Fly Plane
-        </Button>
-    )
+    const menuItems = () => null;
 
-    const menuItems = () => (
-        <MenuItem onClick={() => { alert("I love the Spice Girls!") }}> 
-            <ListItemIcon>
-                <AlbumIcon />
-            </ListItemIcon>
-            <ListItemText primary="Play Album" />
-        </MenuItem>
-    )
-
-    toolbarProps['buttons'] = buttons();
-    toolbarProps['menuItems'] = menuItems();
-    toolbarProps['onClick'] = null;
-    return <PanelToolbar {...toolbarProps} isClosed={false}/>;
+    toolbarProps["buttons"] = buttons();
+    toolbarProps["menuItems"] = menuItems();
+    toolbarProps["onClick"] = null;
+    return <PanelToolbar {...toolbarProps} isClosed={false} />;
 }
