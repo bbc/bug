d10bbc92a695869a6fdb452c695da89586b1a47a
Geoff House <geoff.house@bbc.co.uk>
2021-05-17 15:54:03 +0100

    new db/core for bug-core

--- src/models/panel-buildstatus.js
+++ src/models/panel-buildstatus.js
@@ -1,28 +1,14 @@
 'use strict';
 
 const logger = require('@utils/logger')(module);
-const Db = require('@utils/db');
+const mongoCollection = require("@core/mongo-collection");
 
 //TODO error handling with throw
 
-const connect = async () => {
-    const dbClass = new Db();
-    const db = await dbClass.connect();
-    if(!db) {
-        logger.warning("could not connect to database");
-        return false;
-    }
-    return db;
-}
-
 exports.get = async function(panelId) {
     try {
-        const db = await connect();
-        if(!db) {
-            return null;
-        }
-
-        const result = await db.collection('panelstatus').findOne({"panelid": panelId});
+        const panelStatusCollection = await mongoCollection("panelstatus");
+        const result = await panelStatusCollection.findOne({"panelid": panelId});
         if(result) {
             return result['status'];
         }
@@ -35,12 +21,8 @@ exports.get = async function(panelId) {
 
 exports.set = async function(panelId, statusText, progress) {
     try {
-        const db = await connect();
-        if(!db) {
-            return false;
-        }
-
-        await db.collection('panelstatus').updateOne(
+        const panelStatusCollection = await mongoCollection("panelstatus");
+        await panelStatusCollection.updateOne(
             {
                 "panelid": panelId
             },{
@@ -65,12 +47,8 @@ exports.set = async function(panelId, statusText, progress) {
 
 exports.setError = async function(panelId, errorText) {
     try {
-        let db = await connect();
-        if(!db) {
-            return false;
-        }
-
-        await db.collection('panelstatus').updateOne(
+        const panelStatusCollection = await mongoCollection("panelstatus");
+        await panelStatusCollection.updateOne(
             {
                 "panelid": panelId
             },{
@@ -95,12 +73,8 @@ exports.setError = async function(panelId, errorText) {
 
 exports.setProgress = async function(panelId, progress) {
     try {
-        const db = await connect();
-        if(!db) {
-            return false;
-        }
-
-        await db.collection('panelstatus').updateOne(
+        const panelStatusCollection = await mongoCollection("panelstatus");
+        await panelStatusCollection.updateOne(
             {
                 "panelid": panelId
             },{
@@ -120,13 +94,9 @@ exports.setProgress = async function(panelId, progress) {
 
 exports.list = async function() {
     try {
-        const db = await connect();
-        if(!db) {
-            return null;
-        }
-
+        const panelStatusCollection = await mongoCollection("panelstatus");
         const response = [];
-        const result = await db.collection('panelstatus').find().toArray();
+        const result = await panelStatusCollection.find().toArray();
         for(var eachResult of result) {
             response.push({
                 'panelid': eachResult['panelid'],
