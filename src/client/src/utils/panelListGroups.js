1d5298f078c51df1a83fbffc4a1cd59867cd5400
Geoff House <geoff.house@bbc.co.uk>
2021-05-25 08:46:13 +0100

    don't show empty groups

--- src/client/src/utils/panelListGroups.js
+++ src/client/src/utils/panelListGroups.js
@@ -2,11 +2,13 @@ export default function panelListGroups(panelListData) {
     // sort the panels into groups
     let panelsByGroup = {};
     for (let eachPanel of panelListData) {
-        const group = eachPanel.group ? eachPanel.group : "other";
-        if (!panelsByGroup[group]) {
-            panelsByGroup[group] = [];
+        if (eachPanel.enabled) {
+            const group = eachPanel.group ? eachPanel.group : "other";
+            if (!panelsByGroup[group]) {
+                panelsByGroup[group] = [];
+            }
+            panelsByGroup[group].push(eachPanel);
         }
-        panelsByGroup[group].push(eachPanel);
     }
 
     return panelsByGroup;
