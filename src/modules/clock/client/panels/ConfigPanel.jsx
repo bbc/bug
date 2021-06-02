8240a58be76b473bfa520b3eb0d629d0bd108424
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-06-02 12:26:54 +0100

    Clock update bug fixed

--- src/modules/clock/client/panels/ConfigPanel.jsx
+++ src/modules/clock/client/panels/ConfigPanel.jsx
@@ -1,22 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import { useForm } from "react-hook-form";
 import PanelConfig from "@core/PanelConfig";
 import Loading from "@components/Loading";
+import { useSelector } from "react-redux";
+import PanelGroupFormControl from "@core/PanelGroupFormControl";
+import { useConfigFormHandler } from "@core/ConfigFormHandler";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import { useSelector } from "react-redux";
-
 export default function ConfigPanel() {
     const panelConfig = useSelector((state) => state.panelConfig);
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
 
     if (panelConfig.status === "loading") {
         return <Loading />;
@@ -26,16 +20,29 @@ export default function ConfigPanel() {
         return null;
     }
 
+    const {
+        register,
+        handleSubmit,
+        control,
+        errors,
+        validateServer,
+        messages,
+    } = useConfigFormHandler({
+        panelId: panelConfig.data.id,
+    });
+
     return (
         <>
             <PanelConfig config={panelConfig.data} handleSubmit={handleSubmit}>
                 <Grid item xs={12}>
                     <TextField
-                        inputProps={{ ...register("title", { required: true }) }}
+                        inputProps={{
+                            ...register("title", { required: true }),
+                        }}
                         variant="filled"
                         required
                         fullWidth
-                        error={errors?.title ? true : false}
+                        error={errors.title}
                         defaultValue={panelConfig.data.title}
                         type="text"
                         label="Panel Title"
@@ -47,14 +54,22 @@ export default function ConfigPanel() {
                         inputProps={{ ...register("description") }}
                         variant="filled"
                         fullWidth
-                        error={errors?.description ? true : false}
+                        error={errors.description}
                         defaultValue={panelConfig.data.description}
                         type="text"
                         label="Description"
                     />
                 </Grid>
 
-                <Grid item xs={12}>
+                <Grid item xs={12} md={6}>
+                    <PanelGroupFormControl
+                        name="group"
+                        control={control}
+                        defaultValue={panelConfig.data.group}
+                    />
+                </Grid>
+
+                <Grid item xs={12} md={6}>
                     <FormControl variant="filled" fullWidth>
                         <InputLabel>Type</InputLabel>
                         <Select
@@ -62,7 +77,9 @@ export default function ConfigPanel() {
                             defaultValue={panelConfig.data.type}
                             label="Type"
                             error={errors?.type ? true : false}
-                            inputProps={{ ...register("type", { required: true }) }}
+                            inputProps={{
+                                ...register("type", { required: true }),
+                            }}
                         >
                             <option value={"analogue"}>Analogue</option>
                             <option value={"digital"}>Digital</option>
