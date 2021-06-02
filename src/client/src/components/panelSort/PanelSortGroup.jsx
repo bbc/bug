add69ecb12287370e90b49d9a74366f9b7b7f014
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-06-02 12:27:59 +0100

    Removed unused varaibles

--- src/client/src/components/panelSort/PanelSortGroup.jsx
+++ src/client/src/components/panelSort/PanelSortGroup.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import PanelSortItem from "@components/panelSort/PanelSortItem";
 import clsx from "clsx";
