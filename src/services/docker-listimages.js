fcb25da5c45f189be5b1921aebc46eae0fa19911
Geoff House <geoff.house@bbc.co.uk>
2021-06-17 08:36:39 +0100

    add caching to dockerListImages

--- src/services/docker-listimages.js
+++ src/services/docker-listimages.js
@@ -2,29 +2,37 @@
 
 const logger = require("@utils/logger")(module);
 const docker = require("@utils/docker");
+const cacheStore = require("@core/cache-store");
 
 module.exports = async () => {
-    try {
-        const images = await docker.listImages();
-        let response = [];
-        for (var eachImage of images) {
-            let module = null;
-            if (eachImage.RepoTags && eachImage.RepoTags.length > 0) {
-                module = eachImage.RepoTags[0].split(":")[0];
+    const cacheKey = "dockerListImages";
+
+    // check the cache first
+    let response = cacheStore.get(cacheKey);
+    if (!response) {
+        response = [];
+        try {
+            const images = await docker.listImages();
+            for (var eachImage of images) {
+                let module = null;
+                if (eachImage.RepoTags && eachImage.RepoTags.length > 0) {
+                    module = eachImage.RepoTags[0].split(":")[0];
+                }
+                response.push({
+                    id: eachImage["Id"],
+                    module: module,
+                    tag: eachImage.RepoTags[0],
+                    created: eachImage["Created"],
+                    parentId: eachImage["ParentId"],
+                    size: eachImage["Size"],
+                    virtualSize: eachImage["VirtualSize"],
+                });
             }
-            response.push({
-                id: eachImage["Id"],
-                module: module,
-                tag: eachImage.RepoTags[0],
-                created: eachImage["Created"],
-                parentId: eachImage["ParentId"],
-                size: eachImage["Size"],
-                virtualSize: eachImage["VirtualSize"],
-            });
+        } catch (error) {
+            logger.error(`${error.stack || error.trace || error || error.message}`);
+            throw new Error(`Failed to list images`);
         }
-        return response;
-    } catch (error) {
-        logger.error(`${error.stack || error.trace || error || error.message}`);
-        throw new Error(`Failed to list images`);
+        cacheStore.set(cacheKey, response, 5);
     }
+    return response;
 };
