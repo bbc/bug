c54dacd4eafe0a615cf58c8d6a271e8db53fa2ad
Geoff House <geoff.house@bbc.co.uk>
2021-06-17 08:43:06 +0100

    formatting

--- src/services/docker-listmoduleimages.js
+++ src/services/docker-listmoduleimages.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-const logger = require('@utils/logger')(module);
-const docker = require('@utils/docker');
+const logger = require("@utils/logger")(module);
+const docker = require("@utils/docker");
 
 module.exports = async (moduleName, moduleVersion) => {
     try {
@@ -11,15 +11,13 @@ module.exports = async (moduleName, moduleVersion) => {
         }
 
         const images = await docker.listImages({
-            "filters": {
-                "label": filterLabels
-            }
-
-        })
+            filters: {
+                label: filterLabels,
+            },
+        });
         return images;
-
     } catch (error) {
         logger.warning(`${error.stack || error.trace || error || error.message}`);
         throw new Error(`Failed to get list of any image for module ${moduleName}`);
     }
-}
+};
