fa4981f6771639340a48eadae84f224423770b24
Geoff House <geoff.house@bbc.co.uk>
2021-06-23 14:56:48 +0100

    tidy

--- src/client/src/components/PanelCritical.jsx
+++ src/client/src/components/PanelCritical.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
 import BugAlert from "@components/BugAlert";
 
-const useStyles = makeStyles((theme) => ({
-    alert: {
-        minWidth: "30rem",
-        marginBottom: 8,
-    },
-    title: {
-        color: "#ffffff",
-    },
-}));
-
 export default function PanelCritical({ panel }) {
-    const classes = useStyles();
     return (
         <>
             <Grid container spacing={0} direction="column" alignItems="center" justify="center">
