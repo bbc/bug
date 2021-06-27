99e382ca4a4393dcfcccbccd53a6f9d850ae970d
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-06-27 15:43:38 +0100

    Unused Package Removed

--- src/client/src/components/UserMenuItem.jsx
+++ src/client/src/components/UserMenuItem.jsx
@@ -7,7 +7,6 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
-import Gravatar from "react-gravatar";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import EditIcon from "@material-ui/icons/Edit";
