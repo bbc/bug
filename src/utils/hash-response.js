239ae023350dd05cf52012e0412b14ec3cfb351e
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-06-29 15:30:33 +0100

    User readded to hash-response

--- src/utils/hash-response.js
+++ src/utils/hash-response.js
@@ -1,16 +1,17 @@
-'use strict';
+"use strict";
 
-const md5 = require('md5');
+const md5 = require("md5");
 
-module.exports = (res,req,contents) => {
+module.exports = (res, req, contents) => {
     const response = contents;
     const meta = {
         hash: md5(response),
+        user: req.user,
         request_url: `${req.protocol}://'${req.hostname}${req.originalUrl}`,
         request_method: req.method,
         request_body: req.body,
-    }
+    };
 
     response.meta = meta;
     res.json(response);
-}
+};
