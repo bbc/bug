523a5b8b37f1616ea3a3413249ce9dc6aa87a703
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-06-29 21:51:56 +0100

    APIPoller to Get User Updates

--- src/client/src/data/UserHandler.jsx
+++ src/client/src/data/UserHandler.jsx
@@ -1,37 +1,19 @@
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import userSlice from "@redux/userSlice";
-import axios from "axios";
-// import { useApiPoller } from "@utils/ApiPoller";
+import { useApiPoller } from "@utils/ApiPoller";
 
 // this is used to fetch the initial user state - which may already be logged in
-
 export default function UserHandler(props) {
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        const getUser = async () => {
-            const response = await axios.get(`/api/user/current`);
-            console.log("response", response);
+    const user = useApiPoller({
+        url: `/api/user/current`,
+        interval: 5000,
+    });
 
-            if (response.status === 200) {
-                console.log("response?.data?.status", response?.data?.status);
-                dispatch(userSlice.actions[response?.data?.status](response?.data));
-            }
-        };
-        getUser();
-    }, [dispatch]);
-
-    // const users = useApiPoller({
-    //     url: `/api/system/user`,
-    //     interval: 1000,
-    // });
-
-    // console.log("users", users);
-
-    // if (users.status === "success") {
-    //     dispatch(userSlice.actions[users.status](users));
-    // }
+    if (user.status === "success") {
+        dispatch(userSlice.actions[user.status](user));
+    }
 
     return null;
 }
