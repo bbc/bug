26bcce0deb2737d5c5d8cc2e3c3f0c0f90d70067
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-06-30 09:02:09 +0100

    Tidyup

--- src/client/src/data/StrategiesHandler.jsx
+++ src/client/src/data/StrategiesHandler.jsx
@@ -3,24 +3,24 @@ import { useDispatch } from "react-redux";
 import strategiesSlice from "@redux/strategiesSlice";
 import io from "@utils/io";
 
-const panelList = io("/strategies");
+const strategyList = io("/strategies");
 
 export default function StrategiesHandler(props) {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        panelList.on("connect", () => {
-            // console.log(`${panelList.id}: panelList - subscribed`);
+        strategyList.on("connect", () => {
+            // console.log(`${strategyList.id}: strategyList - subscribed`);
         });
 
-        panelList.on("event", (result) => {
-            // console.log(`${panelList.id}: panelList - event`, result);
+        strategyList.on("event", (result) => {
+            // console.log(`${strategyList.id}: strategyList - event`, result);
             dispatch(strategiesSlice.actions[result["status"]](result));
         });
 
         return async () => {
-            // console.log(`${panelList.id}: panelList - unsubscribed`);
-            panelList.disconnect();
+            // console.log(`${strategyList.id}: strategyList - unsubscribed`);
+            strategyList.disconnect();
         };
     }, [dispatch]);
 
