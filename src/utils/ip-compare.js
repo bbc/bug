9fd0014d1d518b187589726e3db2c70bae9cb259
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-07-02 18:11:14 +0100

    Comapre IP Address Ranges in CIDR Notation

--- src/utils/ip-compare.js
+++ src/utils/ip-compare.js
@@ -1,8 +1,15 @@
 "use strict";
 
-module.exports = async (sourceIP, filterList) => {
-    const status = true;
-    console.log(sourceIP);
-    console.log(filterList);
-    return status;
+const logger = require("@utils/logger")(module);
+const cidrRegex = require("cidr-regex");
+const ipRangeCheck = require("ip-range-check");
+
+module.exports = async (source, ranges) => {
+    for (let range in ranges) {
+        const validRange = await cidrRegex({ exact: true }).test(range);
+        if (validRange) {
+            return ipRangeCheck(source, range);
+        }
+        logger.warning(`${range} is not a valid CIDR notation range`);
+    }
 };
