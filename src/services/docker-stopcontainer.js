9b41b70432e8dc9f0f3c8183165b04d12a4ab13e
Geoff House <geoff.house@bbc.co.uk>
2021-07-05 15:26:50 +0100

    fix error when container not valid

--- src/services/docker-stopcontainer.js
+++ src/services/docker-stopcontainer.js
@@ -1,24 +1,26 @@
-'use strict';
+"use strict";
 
-const logger = require('@utils/logger')(module);
+const logger = require("@utils/logger")(module);
 
 module.exports = async (container) => {
     try {
         logger.info(`stopping container id ${container?.id}`);
 
         return await new Promise((resolve, reject) => {
+            if (!container) {
+                logger.info(`container not valid`);
+                reject();
+            }
             container.stop(function (error, data) {
                 if (error) {
                     if (error.statusCode === 304) {
                         logger.info(`container id ${container.id} already stopped`);
                         resolve(true);
-                    }
-                    else {
+                    } else {
                         logger.warning(`${error.stack || error.trace || error || error.message}`);
                         resolve(false);
                     }
-                }
-                else {
+                } else {
                     logger.info(`container id ${container.id} stopped OK`);
                     resolve(true);
                 }
@@ -28,4 +30,4 @@ module.exports = async (container) => {
         logger.error(`${error.stack || error.trace || error || error.message}`);
         throw new Error(`Failed to stop container id ${container?.id}`);
     }
-}
+};
