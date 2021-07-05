4b80a86b296fbef9044714561afd22f4b9e06498
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2021-07-05 10:01:08 +0100

    teradek status update

--- src/modules/teradek-core/container/services/status-get.js
+++ src/modules/teradek-core/container/services/status-get.js
@@ -1,21 +1,24 @@
 "use strict";
 
-const mongoCollection = require("@core/mongo-collection");
+const statusCheckCollection = require("@core/status-checkcollection");
 
-module.exports = async (index) => {
-    try {
-        const tokenCollection = await mongoCollection("token");
-        const token = await tokenCollection.findOne();
-
-        if (!token) {
-            return {
-                error: "Could not retieve output",
-                status: "error",
-                data: token,
-            };
-        }
-        return { status: "success", data: token };
-    } catch (error) {
-        return null;
-    }
+module.exports = async () => {
+    return [].concat(
+        await statusCheckCollection({
+            collectionName: "token",
+            message: [
+                "There is no tokens for Teradek Core.",
+                "Check your authentication settings and user permissions.",
+            ],
+            itemType: "critical",
+            timeoutSeconds: 1000,
+            flags: ["restartPanel", "configurePanel"],
+        }),
+        await statusCheckCollection({
+            collectionName: "devices",
+            message: "There is no devices avalible.",
+            itemType: "warning",
+            timeoutSeconds: 30,
+        })
+    );
 };
