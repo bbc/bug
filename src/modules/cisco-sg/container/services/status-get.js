648433251cccfb0f61a9927df1fcee6feac5bf2a
Geoff House <geoff.house@bbc.co.uk>
2021-11-29 16:03:59 +0000

    ciscosg: add status checks

--- src/modules/cisco-sg/container/services/status-get.js
+++ src/modules/cisco-sg/container/services/status-get.js
@@ -4,5 +4,26 @@ const statusCheckCollection = require("@core/status-checkcollection");
 const statusCheckPending = require("@services/status-checkpending");
 
 module.exports = async () => {
-    return await statusCheckPending();
+    return [].concat(
+        await statusCheckCollection({
+            collectionName: "interfaces",
+            message: ["There is no recent interface information for this device."],
+            itemType: "critical",
+            timeoutSeconds: 1200,
+            flags: ["restartPanel", "configurePanel"],
+        }),
+        await statusCheckCollection({
+            collectionName: "vlans",
+            message: "VLAN information is out of date.",
+            itemType: "warning",
+            timeoutSeconds: 90,
+        }),
+        await statusCheckCollection({
+            collectionName: "system",
+            message: "System information is out of date.",
+            itemType: "warning",
+            timeoutSeconds: 150,
+        }),
+        await statusCheckPending()
+    );
 };
