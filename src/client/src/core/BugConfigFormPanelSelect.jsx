8e5c63a71c36aa9bd435e88bb6d8186e5d25cc94
Geoff House <geoff.house@bbc.co.uk>
2021-11-30 12:10:24 +0000

    add panel autocomplete control

---
+++ src/client/src/core/BugConfigFormPanelSelect.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import BugHelperText from "@core/BugHelperText";
+import { Controller } from "react-hook-form";
+import { useApiPoller } from "@utils/ApiPoller";
+
+const BugConfigFormPanelSelect = ({
+    name,
+    label,
+    control,
+    defaultValue,
+    children,
+    rules,
+    error,
+    variant,
+    freeSolo,
+    helperText,
+    capability,
+    ...props
+}) => {
+    const panels = useApiPoller({
+        url: `/api/panel/bycapability/${capability}`,
+        interval: 10000,
+    });
+
+    const options =
+        panels.status === "success"
+            ? panels.data.map((panel) => ({
+                  id: panel.id,
+                  label: panel.title,
+              }))
+            : [];
+
+    const processValues = (value) => {
+        let returnArray = [];
+        if (value) {
+            for (let eachValue of value) {
+                const foundObject = options.find((object) => object.id === eachValue);
+                if (foundObject) {
+                    returnArray.push(foundObject);
+                }
+            }
+        }
+        return returnArray;
+    };
+
+    return (
+        <>
+            <FormControl {...props}>
+                <Controller
+                    render={({ field: { onChange, onBlur, value } }) => {
+                        return (
+                            <Autocomplete
+                                multiple
+                                filterSelectedOptions
+                                options={options}
+                                onBlur={onBlur}
+                                onChange={(event, values) => {
+                                    const returnValues = [];
+                                    for (let eachValue of values) {
+                                        if (eachValue.id) {
+                                            returnValues.push(eachValue.id);
+                                        }
+                                    }
+                                    onChange(returnValues);
+                                }}
+                                defaultValue={processValues(value)}
+                                value={processValues(value)}
+                                renderInput={(params) => (
+                                    <TextField {...params} variant="standard" label={label} error={error} />
+                                )}
+                            />
+                        );
+                    }}
+                    name={name}
+                    control={control}
+                    defaultValue={defaultValue}
+                    rules={rules}
+                />
+            </FormControl>
+            {helperText && <BugHelperText>{helperText}</BugHelperText>}
+        </>
+    );
+};
+export default BugConfigFormPanelSelect;
