c66b831680f1c10c04448cf533ee44dcbd7c8efd
Geoff House <geoff.house@bbc.co.uk>
2021-12-06 21:28:05 +0000

    tidy

--- src/modules/mikrotik-dhcp/container/services/status-get.js
+++ src/modules/mikrotik-dhcp/container/services/status-get.js
@@ -3,14 +3,16 @@
 const statusCheckCollection = require("@core/status-checkcollection");
 
 module.exports = async () => {
-    return await statusCheckCollection({
-        collectionName: "leases",
-        message: [
-            "There is no recent router data for this device.",
-            "Check your connection and authentication settings.",
-        ],
-        itemType: "critical",
-        timeoutSeconds: 10,
-        flags: ["restartPanel", "configurePanel"],
-    });
+    return [].concat(
+        await statusCheckCollection({
+            collectionName: "leases",
+            message: [
+                "There is no recent router data for this device.",
+                "Check your connection and authentication settings.",
+            ],
+            itemType: "critical",
+            timeoutSeconds: 10,
+            flags: ["restartPanel", "configurePanel"],
+        })
+    );
 };
