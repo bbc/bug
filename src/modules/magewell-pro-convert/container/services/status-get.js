3e06759639a0e7b435f8788581de12f777c4f947
Geoff House <geoff.house@bbc.co.uk>
2021-12-06 09:06:35 +0000

    remove tags from magewell 'no sources' alert

--- src/modules/magewell-pro-convert/container/services/status-get.js
+++ src/modules/magewell-pro-convert/container/services/status-get.js
@@ -3,16 +3,12 @@
 const statusCheckCollection = require("@core/status-checkcollection");
 
 module.exports = async () => {
-    // const seconds = new Date().getTime() / 1000;
-    // const a = Math.round(seconds / 6);
-    // if (a % 2 == 0) {
     return [].concat(
         await statusCheckCollection({
             collectionName: "sources",
             message: ["There are no NDI sources for this device.", "Check the network or discovery server."],
             itemType: "warning",
             timeoutSeconds: 30,
-            flags: ["restartPanel", "configurePanel"],
         })
     );
 };
