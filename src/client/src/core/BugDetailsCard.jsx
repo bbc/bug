261d2ac90658ec5d1cef1d6cd807603dd3e6670a
Geoff House <geoff.house@bbc.co.uk>
2021-12-10 09:26:33 +0000

    add expand feature to bugdetailscard

--- src/client/src/core/BugDetailsCard.jsx
+++ src/client/src/core/BugDetailsCard.jsx
@@ -3,8 +3,34 @@ import Card from "@mui/material/Card";
 import Box from "@mui/material/Box";
 import CardHeader from "@mui/material/CardHeader";
 import BugDetailsTable from "@core/BugDetailsTable";
+import Collapse from "@mui/material/Collapse";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
 
-export default function BugDetailsCard({ title, width, data, ...props }) {
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+const MemoizedBugDetailsTable = ({ width, data }) => {
+    console.log("memo");
+    return React.useMemo(() => <BugDetailsTable width={width} gridLines={false} data={data} />, [data]);
+};
+
+function BugDetailsCard({ title, width, data, collapsible = false, collapsed = false, ...props }) {
+    const [expanded, setExpanded] = React.useState(!collapsed);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
         <Card
             sx={{
@@ -23,15 +49,33 @@ export default function BugDetailsCard({ title, width, data, ...props }) {
                             fontWeight: 500,
                             color: "#ffffff",
                         },
+                        height: "49px",
+                        padding: "8px 16px",
                     }}
                     title={title}
+                    action={
+                        collapsible && (
+                            <ExpandMore
+                                expand={expanded}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon />
+                            </ExpandMore>
+                        )
+                    }
                 />
             )}
             {data && (
-                <Box sx={{ margin: "2px" }}>
-                    <BugDetailsTable width={width} gridLines={false} data={data} />
-                </Box>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <Box sx={{ margin: "2px" }}>
+                        <MemoizedBugDetailsTable width={width} gridLines={false} data={data} />
+                    </Box>
+                </Collapse>
             )}
         </Card>
     );
 }
+
+export default BugDetailsCard;
