670b996fa42b320ce4c558f2431cf13b22d70f47
Geoff House <geoff.house@bbc.co.uk>
2021-12-13 15:06:50 +0000

    allow formatbps to return object

--- src/core/format-bps.js
+++ src/core/format-bps.js
@@ -1,4 +1,4 @@
-module.exports = (bits, decimals) => {
+module.exports = (bits, decimals, asObject = false) => {
     if (bits === 0) return "0";
     if (bits < 0) {
         bits = bits * -1;
@@ -12,5 +12,12 @@ module.exports = (bits, decimals) => {
 
     const i = Math.floor(Math.log(bits) / Math.log(k));
     const size = sizes[i] ? sizes[i] : sizes[0];
-    return parseFloat((bits / Math.pow(k, i)).toFixed(dm)) + " " + size;
+    if (asObject) {
+        return {
+            value: parseFloat((bits / Math.pow(k, i)).toFixed(dm)),
+            label: size,
+        };
+    } else {
+        return parseFloat((bits / Math.pow(k, i)).toFixed(dm)) + " " + size;
+    }
 };
