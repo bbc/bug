70bdc735ce2592e873bde163a96e04003215aec7
Ryan McCartney <ryan.mccartney01@bbc.co.uk>
2021-12-14 23:43:45 +0000

    RSS Feed Module

---
+++ src/modules/rss-feed/client/components/FeedCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TimeAgo from "javascript-time-ago";
+
+export default function FeedCard({ item }) {
+    const timeAgo = new TimeAgo("en-GB");
+
+    const handleDetails = (event) => {
+        window.open(item?.link);
+    };
+
+    return (
+        <>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        {timeAgo.format(Date.now() - new Date(item?.created / 1000))}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        {item?.title}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {item?.author}
+                    </Typography>
+                    <Typography variant="body2"> {item?.description}</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button onClick={handleDetails} size="small">
+                        Details
+                    </Button>
+                </CardActions>
+            </Card>
+        </>
+    );
+}
---
+++ src/modules/rss-feed/client/panels/MainPanel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useApiPoller } from "@utils/ApiPoller";
+import Loading from "@components/Loading";
+import FeedCard from "./../components/FeedCard";
+import Grid from "@mui/material/Grid";
+
+export default function MainPanel() {
+    const params = useParams();
+
+    const items = useApiPoller({
+        url: `/container/${params.panelId}/feed/items`,
+        interval: 60000,
+    });
+
+    const getCards = (items) => {
+        const cards = [];
+        for (let item of items) {
+            cards.push(
+                <>
+                    <Grid item lg={6} xs={12}>
+                        <FeedCard key={item._id} item={item} />
+                    </Grid>
+                </>
+            );
+        }
+        return cards;
+    };
+
+    if (items.status === "loading" || items.status === "idle") {
+        return <Loading />;
+    }
+
+    return (
+        <>
+            <Grid container spacing={4}>
+                {getCards(items.data)}
+            </Grid>
+        </>
+    );
+}
