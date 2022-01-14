f81026052088d971e0c9858553393c3c88aa111d
Geoff House <geoff.house@bbc.co.uk>
2022-01-14 08:22:31 +0000

    mikrotikdhcp - delete lease from db too

--- src/modules/mikrotik-dhcp/container/services/mikrotik-leasedelete.js
+++ src/modules/mikrotik-dhcp/container/services/mikrotik-leasedelete.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mikrotikConnect = require("../utils/mikrotik-connect");
+const mongoCollection = require("@core/mongo-collection");
 
 module.exports = async (leaseId) => {
     const conn = await mikrotikConnect();
@@ -12,6 +13,11 @@ module.exports = async (leaseId) => {
         await conn.write("/ip/dhcp-server/lease/remove", ["=numbers=" + leaseId]);
         console.log(`mikrotik-leasedelete: removed lease id ${leaseId}`);
         conn.close();
+
+        // now update local db
+        const dbLeases = await mongoCollection("leases");
+        await dbLeases.deleteOne({ id: leaseId });
+
         return true;
     } catch (error) {
         console.log(`mikrotik-leasedelete: ${error.stack || error.trace || error || error.message}`);
