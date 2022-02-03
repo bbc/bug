1453ff64cd472cfebb06ce91a65dd3aa3bc438cd
Geoff House <geoffhouse@users.noreply.github.com>
2022-02-03 14:44:50 +0000

    fix error when no cookies available

--- src/client/src/hooks/CookieId.jsx
+++ src/client/src/hooks/CookieId.jsx
@@ -1,10 +1,14 @@
 import { useLocation } from "react-router-dom";
 
 const useCookieId = (id) => {
-    const location = useLocation();
-    let pathname = location.pathname.replace(/^\/|\/$/g, "");
-    pathname = pathname.replace("/", "_");
-    return `${pathname}_${id}`;
+    try {
+        const location = useLocation();
+        let pathname = location.pathname.replace(/^\/|\/$/g, "");
+        pathname = pathname.replace("/", "_");
+        return `${pathname}_${id}`;
+    } catch (error) {
+        return "NONE";
+    }
 };
 
 export { useCookieId };
