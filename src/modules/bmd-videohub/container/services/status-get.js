c0e973ce76893df4be6385e0dcfcc4d926e1fc3f
Geoff House <geoffhouse@users.noreply.github.com>
2022-02-11 10:50:27 +0000

    videohub: add alarms

--- src/modules/bmd-videohub/container/services/status-get.js
+++ src/modules/bmd-videohub/container/services/status-get.js
@@ -1,30 +1,17 @@
 "use strict";
 
 const statusCheckCollection = require("@core/status-checkcollection");
-const statusCheckWorkers = require("@core/status-checkworkers");
+const statusCheckAlarms = require("./status-checkalarms");
 
 module.exports = async () => {
-    // const seconds = new Date().getTime() / 1000;
-    // const a = Math.round(seconds / 6);
-    // if (a % 2 == 0) {
     return [].concat(
         await statusCheckCollection({
             collectionName: "data",
             message: ["There is no recent router data for this device.", "Check your connection or address details."],
             itemType: "critical",
-            timeoutSeconds: 15,
+            timeoutSeconds: 11115,
             flags: ["restartPanel", "configurePanel"],
-        })
+        }),
+        await statusCheckAlarms()
     );
-    // } else {
-    //     return [].concat(
-    //         await statusCheckCollection({
-    //             collectionName: "data",
-    //             message: "There is no recent router data for this device.\nCheck your connection or address details.",
-    //             itemType: "critical",
-    //             timeoutSeconds: 10,
-    //             flags: ["restartPanel", "configurePanel"],
-    //         })
-    //     );
-    // }
 };
