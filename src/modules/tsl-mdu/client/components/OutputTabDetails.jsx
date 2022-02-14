add0840d098a429f8da93dd296e7306ab57aad04
Geoff House <geoffhouse@users.noreply.github.com>
2022-02-14 08:31:44 +0000

    tsmdu: use BugDetailsTable for output

--- src/modules/tsl-mdu/client/components/OutputTabDetails.jsx
+++ src/modules/tsl-mdu/client/components/OutputTabDetails.jsx
@@ -1,56 +1,35 @@
 import React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import BugDetailsTable from "@core/BugDetailsTable";
 
 export default function OutputTabDetails({ output, panelId }) {
     return (
         <>
             <Grid item xs={12}>
-                <TableContainer component={Paper} square>
-                    <Table
-                        sx={{
-                            width: "14rem",
-                            "@media (max-width:512px)": {
-                                width: "10rem",
-                            },
-                        }}
-                        aria-label="simple table"
-                    >
-                        <TableBody>
-                            <TableRow>
-                                <TableCell variant="head">Number</TableCell>
-                                <TableCell>{output.number}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell variant="head" className={classes.tableName}>
-                                    Name
-                                </TableCell>
-                                <TableCell>{output.name}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell variant="head">State</TableCell>
-                                <TableCell>{output.state ? "On" : "Off"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell variant="head">Fuse</TableCell>
-                                <TableCell>{output.fuse}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell variant="head">Delay</TableCell>
-                                <TableCell>{output.delay}s</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell variant="head">SNMP Lock</TableCell>
-                                <TableCell>{output.lock ? "On" : "Off"}</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <BugDetailsTable
+                    items={[
+                        {
+                            name: "Number",
+                            value: output.number,
+                        },
+                        {
+                            name: "Name",
+                            value: output.name,
+                        },
+                        {
+                            name: "State",
+                            value: output.state ? "On" : "Off",
+                        },
+                        {
+                            name: "Fuse",
+                            value: output.fuse,
+                        },
+                        {
+                            name: "Delay",
+                            value: `${output.delay}s`,
+                        },
+                    ]}
+                />
             </Grid>
         </>
     );
