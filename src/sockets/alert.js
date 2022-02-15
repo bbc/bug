7ee7a7b8f7cfbcf2fd77db2950c0b07d1b0ec9c9
Ryan McCartney <ryan.mccartney01@bbc.co.uk>
2022-02-15 11:10:30 +0000

    Log userId and panelId of an alert

--- src/sockets/alert.js
+++ src/sockets/alert.js
@@ -3,7 +3,7 @@ const logger = require("@utils/logger")(module);
 const alertHandler = (namespace, socket) => {
     socket.on("event", (data) => {
         socket.broadcast.emit("event", data);
-        logger.action(data.message);
+        logger.action(data.message, { userId: data?.userId, panelId: data?.panelId });
     });
 };
 
