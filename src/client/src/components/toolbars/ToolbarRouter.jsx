d10bd22f95a95be00fb77b1f3c85ad9e51a7e65a
Geoff House <geoffhouse@users.noreply.github.com>
2022-02-15 08:51:09 +0000

    add commented-out console.log line to fix cache

--- src/client/src/components/toolbars/ToolbarRouter.jsx
+++ src/client/src/components/toolbars/ToolbarRouter.jsx
@@ -35,6 +35,7 @@ const ToolbarRouter = (props) => {
         }
 
         if (Toolbars["modules"][panelConfig?.data?.module]) {
+            // console.log("Uncomment this line in development to force a refresh of the Toolbar include logic above");
             const Toolbar = Toolbars["modules"][panelConfig.data.module]["client"]["Toolbar"];
             return <Toolbar panelId={panelConfig.data.id} />;
         }
