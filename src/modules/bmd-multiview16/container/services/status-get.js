71b2b4583b1d145dcf037277f0a547aa90baa23c
Geoff House <geoffhouse@users.noreply.github.com>
2022-02-21 06:22:23 +0000

    bmd-multiview16: fix status + timeout

--- src/modules/bmd-multiview16/container/services/status-get.js
+++ src/modules/bmd-multiview16/container/services/status-get.js
@@ -3,14 +3,13 @@
 const statusCheckCollection = require("@core/status-checkcollection");
 
 module.exports = async () => {
-    return [];
-    // .concat(
-    //     await statusCheckCollection({
-    //         collectionName: "data",
-    //         message: ["There is no recent router data for this device.", "Check your connection or address details."],
-    //         itemType: "critical",
-    //         timeoutSeconds: 11115,
-    //         flags: ["restartPanel", "configurePanel"],
-    //     })
-    // );
+    return [].concat(
+        await statusCheckCollection({
+            collectionName: "videohub_device",
+            message: ["There is no recent data for this device.", "Check your connection or address details."],
+            itemType: "critical",
+            timeoutSeconds: 15,
+            flags: ["restartPanel", "configurePanel"],
+        })
+    );
 };
