0ad8d315b16ff13074533c731a8bab14d61874c4
Geoff House <geoffhouse@users.noreply.github.com>
2022-03-01 07:42:53 +0000

    codec-apipoller: add codecdb capability handler

--- src/modules/codec-apipoller/container/api/app.js
+++ src/modules/codec-apipoller/container/api/app.js
@@ -8,6 +8,7 @@ const configRouter = require("./routes/config");
 const defaultRouter = require("@routes/default");
 const statusRouter = require("./routes/status");
 const codecRouter = require("@routes/codec");
+const capabilitiesRouter = require("@routes/capabilities");
 
 let app = express();
 
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use("/api/config", configRouter);
 app.use("/api/status", statusRouter);
 app.use("/api/codec", codecRouter);
+app.use("/api/capabilities", capabilitiesRouter);
 
 app.use("*", defaultRouter);
 
