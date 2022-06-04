ef3ebba229fd62fa9f5046e15e00ee62542a581d
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2022-06-04 15:26:12 +0100

    Map view for Pinger

--- src/modules/pinger/container/services/host-list.js
+++ src/modules/pinger/container/services/host-list.js
@@ -1,12 +1,19 @@
 "use strict";
 
 const mongoCollection = require("@core/mongo-collection");
+const configGet = require("@core/config-get");
 
 module.exports = async () => {
     try {
+        const config = await configGet();
         const hostsCollection = await mongoCollection("hosts");
         const hosts = await hostsCollection.find().toArray();
-        return await hosts;
+
+        const mergedHosts = hosts.map((host) => {
+            return { ...host, ...config.hosts[host.hostId] };
+        });
+
+        return await mergedHosts;
     } catch (error) {
         return [];
     }
