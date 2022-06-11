3907cb4ca01fe7f81ddcaf6e40d36d5ae6f052f2
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2022-06-11 18:56:36 +0100

    Peer Dependancy Update

--- src/modules/rss-feed/client/components/FeedCard.jsx
+++ src/modules/rss-feed/client/components/FeedCard.jsx
@@ -5,7 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import TimeAgo from "javascript-time-ago";
-import ReactHtmlParser from "react-html-parser";
+import parse from "html-react-parser";
 
 export default function FeedCard({ item }) {
     const timeAgo = new TimeAgo("en-GB");
@@ -27,7 +27,7 @@ export default function FeedCard({ item }) {
                     <Typography sx={{ mb: 1.5 }} color="text.secondary">
                         {item?.author}
                     </Typography>
-                    <Typography variant="body2"> {ReactHtmlParser(item?.description)}</Typography>
+                    <Typography variant="body2"> {parse(item?.description)}</Typography>
                 </CardContent>
                 <CardActions>
                     <Button onClick={handleDetails} size="small">
