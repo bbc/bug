7dbd82d13e1422736b4a52421d2be0954a4d0798
Geoff House <geoffhouse@users.noreply.github.com>
2022-07-12 10:14:47 +0100

    fix storybook with emotion 11

--- src/client/.storybook/main.js
+++ src/client/.storybook/main.js
@@ -1,6 +1,20 @@
 const path = require("path");
+const fs = require("fs");
+const { merge } = require("webpack-merge");
 
-console.log(path.resolve("src/core"));
+function getPackageDir(filepath) {
+    let currDir = path.dirname(require.resolve(filepath));
+    while (true) {
+        if (fs.existsSync(path.join(currDir, "package.json"))) {
+            return currDir;
+        }
+        const { dir, root } = path.parse(currDir);
+        if (dir === root) {
+            throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+        }
+        currDir = dir;
+    }
+}
 
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -20,6 +34,9 @@ module.exports = {
                 "@redux": path.resolve("src/redux"),
                 "@utils": path.resolve("src/utils"),
                 "@hooks": path.resolve("src/hooks"),
+                "@emotion/core": getPackageDir("@emotion/react"),
+                "@emotion/styled": getPackageDir("@emotion/styled"),
+                "emotion-theming": getPackageDir("@emotion/react"),
             },
         },
     }),
