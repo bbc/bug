fbb661216d90d608ede872e737aa135adca6ce49
Geoff House <geoffhouse@users.noreply.github.com>
2022-08-31 14:05:20 +0100

    comrex-briclink: fix call time

--- src/modules/comrex-briclink/client/components/Statistics.jsx
+++ src/modules/comrex-briclink/client/components/Statistics.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import { useApiPoller } from "@hooks/ApiPoller";
 import BugDetailsCard from "@core/BugDetailsCard";
 import formatBps from "@utils/format-bps";
+import TimeAgo from "javascript-time-ago";
 
 export default function InterfaceTabDetails({ panelId, interfaceId }) {
+    const timeAgo = new TimeAgo("en-GB");
+
     const stats = useApiPoller({
         url: `/container/${panelId}/statistics/`,
         interval: 5000,
     });
+
+    console.log(stats.data?.callTime);
 
     return (
         <BugDetailsCard
             elevation={0}
             items={[
                 { name: "Remote Unit", value: stats.data?.name ? stats.data?.name : "NOT CONNECTED" },
-                { name: "Duration", value: stats.data?.callTime ? `${stats.data?.callTime} seconds` : "" },
+                {
+                    name: "Call time",
+                    value: stats.data?.callTime ? `${timeAgo.format(Date.now() - stats.data?.callTime * 1000)}` : "",
+                },
                 { name: "RX Rate", value: stats.data?.effRxBitrate ? formatBps(stats.data?.effRxBitrate) : "" },
                 { name: "RX Overhead", value: stats.data?.rxOverhead ? formatBps(stats.data?.rxOverhead) : "" },
                 { name: "RX Delay", value: stats.data?.delayIn ? `${stats.data?.delayIn} ms` : "" },
