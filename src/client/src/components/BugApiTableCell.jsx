fe827fb2f275f802ac8a212bd6d8a457f8357836
Geoff House <geoffhouse@users.noreply.github.com>
2022-10-03 18:47:46 +0100

    fix lnvalid property

--- src/client/src/components/BugApiTableCell.jsx
+++ src/client/src/components/BugApiTableCell.jsx
@@ -12,7 +12,7 @@ export default function BugApiTableCell({ children, column, index, sx, ...props 
                 maxWidth: column.maxWidth ? column.maxWidth : column.noWrap ? "0px" : "none",
                 overflow: column.noWrap ? "hidden" : "visible",
                 textAlign: column.align ? column.align : "start",
-                textOverflow: column.noWrap ? "none" : "clip",
+                textOverflow: column.noWrap ? "inherit" : "clip",
                 whiteSpace: column.noWrap ? "nowrap" : "normal",
                 paddingLeft: column.noPadding ? (index === 0 ? "8px" : "4px") : "12px",
                 paddingRight: column.noPadding ? "4px" : "12px",
