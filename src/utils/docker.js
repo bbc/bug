c8f6c39227dc8446dd6a3ec49951ce339c9d3ddc
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2022-10-04 00:28:08 +0100

    Additional docker connection options

--- src/utils/docker.js
+++ src/utils/docker.js
@@ -4,9 +4,15 @@
 
 const Docker = require("dockerode");
 const socketPath = process.env.DOCKER_SOCKET_PATH || "/var/run/docker.sock";
+const dockerHost = process.env.DOCKER_HOST;
+const dockerPort = process.env.DOCKER_PORT || 2375;
 
-let docker = new Docker({ socketPath: socketPath });
+let docker;
 
-console.log(docker)
+if (dockerHost && dockerPort) {
+    docker = new Docker({ host: dockerHost, port: dockerPort }); //defaults to http
+} else {
+    docker = new Docker({ socketPath: socketPath });
+}
 
 module.exports = docker;
