9a0d458f0e58c93020c4f0449c10dd8da5417b06
Geoff House <geoffhouse@users.noreply.github.com>
2022-10-12 09:28:25 +0100

    tieline-merlin: fix when no stats

--- src/modules/tieline-merlin/container/services/group-getstatistics.js
+++ src/modules/tieline-merlin/container/services/group-getstatistics.js
@@ -6,9 +6,11 @@ module.exports = async (groupId) => {
     // and now the stats (what we actually want!)
     const statisticsCollection = await mongoCollection("statistics");
     const stats = await statisticsCollection.findOne({ type: "group", groupId: groupId });
-    stats["_time"] = "";
-    if (stats["seconds"]) {
-        stats["_time"] = new Date(stats["seconds"] * 1000).toISOString().substring(11, 19);
+    if (stats) {
+        stats["_time"] = "";
+        if (stats["seconds"]) {
+            stats["_time"] = new Date(stats["seconds"] * 1000).toISOString().substring(11, 19);
+        }
     }
     return stats;
 };
