0ddd1ef76c970dc48080115d06a145f13beaafae
Geoff House <geoffhouse@users.noreply.github.com>
2022-11-30 12:32:24 +0000

    add color customization to bugpowericon

--- src/client/src/core/BugPowerIcon.jsx
+++ src/client/src/core/BugPowerIcon.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import PowerSettingsNew from "@mui/icons-material/PowerSettingsNew";
 
-export default function BugPowerIcon({ disabled = false }) {
+export default function BugPowerIcon({
+    disabled = false,
+    sx,
+    disabledColor = "#ffffff",
+    activeColor = "primary.main",
+}) {
     return (
         <PowerSettingsNew
             sx={{
-                color: disabled ? "#ffffff" : "primary.main",
+                ...sx,
+                color: disabled ? disabledColor : activeColor,
                 opacity: disabled ? 0.1 : 1,
                 display: "block",
                 margin: "auto",
