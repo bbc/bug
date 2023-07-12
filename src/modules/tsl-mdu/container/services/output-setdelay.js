18dd47d27c5b1053b584e9a10043d02fd5a9e46e
Geoff House <geoff.house@bbc.co.uk>
2023-07-12 09:29:47 +0100

    improve tsl-mdu when updating backend

--- src/modules/tsl-mdu/container/services/output-setdelay.js
+++ src/modules/tsl-mdu/container/services/output-setdelay.js
@@ -2,6 +2,7 @@
 
 const MDU = require("@utils/mdu");
 const configGet = require("@core/config-get");
+const mongoCollection = require("@core/mongo-collection");
 
 module.exports = async (index, delay) => {
     const config = await configGet();
@@ -14,6 +15,12 @@ module.exports = async (index, delay) => {
 
     if (response.status !== 200) {
         console.log(response.output);
+        return false;
     }
+
+    // update the db
+    const outputsCollection = await mongoCollection("outputs");
+    await outputsCollection.updateOne({ number: parseInt(index) }, { $set: { delay: parseInt(delay) } });
+
     return true;
 };
