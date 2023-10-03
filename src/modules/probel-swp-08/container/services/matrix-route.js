1d202940ebe60a842c04dc86bbe2e02c58a73419
Ryan McCartney <ryan@mccartney.info>
2023-10-03 11:25:14 +0100

    Update probel module and add routing service

--- src/modules/probel-swp-08/container/services/matrix-route.js
+++ src/modules/probel-swp-08/container/services/matrix-route.js
@@ -1,26 +1,13 @@
 "use strict";
 
-const configGet = require("@core/config-get");
 const matrix = require("@utils/matrix");
 const logger = require("@core/logger")(module);
 
 module.exports = async (destinatonIndex, sourceIndex) => {
-    let config;
     try {
-        config = await configGet();
-        if (!config) {
-            throw new Error();
-        }
-    } catch (error) {
-        logger.error(`matrix-route: failed to fetch config`);
-        return false;
-    }
-
-    try {
-        const router = new matrix({ port: config.port, host: config.address });
-        await router.connect();
-        await router.send("VIDEO OUTPUT ROUTING", `${destinatonIndex} ${sourceIndex}`);
-        return true;
+        const status = await matrix.routeAllLevels(sourceIndex + 1, destinatonIndex + 1);
+        logger.info(`Routed Source #${sourceIndex + 1} to Destination #${destinatonIndex + 1} on all levels.`);
+        return status;
     } catch (error) {
         logger.error("matrix-route: ", error);
         return false;
