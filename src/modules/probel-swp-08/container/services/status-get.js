9281c00de5733607823f8b7810e966379373d7d6
Ryan McCartney <ryan@mccartney.info>
2023-10-29 12:07:53 +0000

    Update status-get in probel module

--- src/modules/probel-swp-08/container/services/status-get.js
+++ src/modules/probel-swp-08/container/services/status-get.js
@@ -1,15 +1,5 @@
 "use strict";
 
-const statusCheckCollection = require("@core/status-checkcollection");
-
 module.exports = async () => {
-    return [].concat(
-        await statusCheckCollection({
-            collectionName: "data",
-            message: ["There is no recent router data for this device.", "Check your connection or address details."],
-            itemType: "critical",
-            timeoutSeconds: 3000,
-            flags: ["restartPanel", "configurePanel"],
-        })
-    );
+    return [];
 };
