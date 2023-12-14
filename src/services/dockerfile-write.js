f43a33c86cc76b935a0ab08422c4c89b76325eef
Ryan McCartney <ryan@mccartney.info>
2023-12-14 20:11:49 +0000

    Updated to Node 20 LTS

--- src/services/dockerfile-write.js
+++ src/services/dockerfile-write.js
@@ -11,7 +11,7 @@ module.exports = async (filepath) => {
 
         const filename = path.join(filepath, "Dockerfile");
         const fileArray = [
-            "FROM node:16.19-alpine",
+            "FROM node:20-alpine",
             `WORKDIR ${moduleHome}`,
             `COPY . .`,
             `RUN apk add iputils`, //Added due to issue - https://github.com/danielzzz/node-ping/issues/89
