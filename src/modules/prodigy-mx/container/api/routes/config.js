c816a60618f484eb6f29c586937125611f2d2f34
Geoff House <geoff.house@bbc.co.uk>
2024-03-08 18:17:25 +0000

    add base module

---
+++ src/modules/prodigy-mx/container/api/routes/config.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const configGet = require("@core/config-get");
+const configPut = require("@core/config-put");
+
+router.get("/", async function (req, res, next) {
+    try {
+        res.json({
+            status: "success",
+            data: await configGet(),
+        });
+    } catch (error) {
+        res.json({
+            status: "error",
+            message: "Failed to fetch panel config",
+        });
+    }
+});
+
+// this is the endpoint used to update the config in the container
+// it shouldn't be used by the module client itself
+router.put("/", async function (req, res, next) {
+    try {
+        res.json({
+            status: "success",
+            data: await configPut(req.workers, req.body),
+        });
+    } catch (error) {
+        res.json({
+            status: "error",
+            message: "Failed to update panel config",
+        });
+    }
+});
+
+module.exports = router;
---
+++ src/modules/prodigy-mx/container/services/status-get.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// const statusCheckCollection = require("@core/status-checkcollection");
+
+module.exports = async () => {
+    return [];
+    // return [].concat(
+    // await statusCheckCollection({
+    //     collectionName: "myCollection",
+    //     message: ["There is no recent wibble data for this device."],
+    //     itemType: "critical",
+    //     timeoutSeconds: 1200,
+    //     flags: ["restartPanel", "configurePanel"],
+    // })
+    // );
+};
