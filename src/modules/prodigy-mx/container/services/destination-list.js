39f4f5b108ab360262d1b2cd8680f000a6d97c7b
Geoff House <geoff.house@bbc.co.uk>
2024-05-25 19:49:47 +0100

    prodigy-mx: show error when no data

--- src/modules/prodigy-mx/container/services/destination-list.js
+++ src/modules/prodigy-mx/container/services/destination-list.js
@@ -16,38 +16,38 @@ module.exports = async (groupIndex = null, showExcluded = false) => {
         return false;
     }
 
-    const icons = config.destinationIcons ? config.destinationIcons : [];
-    const iconColors = config.destinationIconColors ? config.destinationIconColors : [];
-
-    const outputArray = {
-        groups: [],
-        destinations: [],
-    };
-
-    const groups = await groupList("destination");
-
-    // add groups to output array
-    outputArray["groups"] = groups.map((eachGroup, eachIndex) => {
-        // we only need a subset of the fields - eg we don't need the huge value array
-        return {
-            label: eachGroup.label,
-            index: eachGroup.index,
-            fixed: eachGroup.fixed,
-            selected: eachIndex === groupIndex,
-        };
-    });
-
-    const buttonsFixed = groups.find((group, index) => index === groupIndex)?.fixed ?? false;
-
-    // then calculate valid destinations for this group
-    const validDestinations = groups[groupIndex] ? groups[groupIndex]["value"] : [];
-
     // get get the existing data from the db
     const outputLabels = await mongoSingle.get("output_labels");
     const inputLabels = await mongoSingle.get("input_labels");
     const routing = await mongoSingle.get("routing");
 
     if (outputLabels && routing && inputLabels) {
+        const outputArray = {
+            groups: [],
+            destinations: [],
+        };
+
+        const icons = config.destinationIcons ? config.destinationIcons : [];
+        const iconColors = config.destinationIconColors ? config.destinationIconColors : [];
+
+        const groups = await groupList("destination");
+
+        // add groups to output array
+        outputArray["groups"] = groups.map((eachGroup, eachIndex) => {
+            // we only need a subset of the fields - eg we don't need the huge value array
+            return {
+                label: eachGroup.label,
+                index: eachGroup.index,
+                fixed: eachGroup.fixed,
+                selected: eachIndex === groupIndex,
+            };
+        });
+
+        const buttonsFixed = groups.find((group, index) => index === groupIndex)?.fixed ?? false;
+
+        // then calculate valid destinations for this group
+        const validDestinations = groups[groupIndex] ? groups[groupIndex]["value"] : [];
+
         outputArray["destinations"] = outputLabels
             .filter((labelItem, index) => validDestinations.includes(index))
             .map((labelItem) => {
@@ -67,10 +67,10 @@ module.exports = async (groupIndex = null, showExcluded = false) => {
                     iconColor: iconColors[labelIndex] ? iconColors[labelIndex] : "#ffffff",
                 };
             });
+        // sort by order field
+        outputArray["destinations"].sort((a, b) => (a.order > b.order ? 1 : -1));
+
+        return outputArray;
     }
-
-    // sort by order field
-    outputArray["destinations"].sort((a, b) => (a.order > b.order ? 1 : -1));
-
-    return outputArray;
+    return null;
 };
--- src/modules/prodigy-mx/container/services/source-list.js
+++ src/modules/prodigy-mx/container/services/source-list.js
@@ -16,13 +16,17 @@ module.exports = async (destinationIndex = null, groupIndex = 0) => {
         return false;
     }
 
-    const icons = config.sourceIcons ? config.sourceIcons : [];
-    const iconColors = config.sourceIconColors ? config.sourceIconColors : [];
+    const inputLabels = await mongoSingle.get("input_labels");
+    if (!inputLabels) {
+        return null;
+    }
 
     const outputArray = {
         groups: [],
         sources: [],
     };
+    const icons = config.sourceIcons ? config.sourceIcons : [];
+    const iconColors = config.sourceIconColors ? config.sourceIconColors : [];
 
     const groups = await groupList("source");
 
@@ -50,36 +54,33 @@ module.exports = async (destinationIndex = null, groupIndex = 0) => {
         selectedSourceIndex = routing?.[destinationIndex][1];
     }
 
-    const inputLabels = await mongoSingle.get("input_labels");
-
-    if (inputLabels) {
-        outputArray["sources"] = inputLabels
-            .filter((labelItem, index) => validSources.includes(index))
-            .map((labelItem) => {
-                const labelIndex = labelItem[0];
-                return {
-                    index: labelIndex,
-                    label: labelItem[1],
-                    fixed: buttonsFixed,
-                    selected: selectedSourceIndex === labelIndex,
-                    order: groupIndex !== null ? validSources.indexOf(labelIndex) : labelIndex,
-                    icon: icons[labelIndex] ? icons[labelIndex] : null,
-                    iconColor: iconColors[labelIndex] ? iconColors[labelIndex] : "#ffffff",
-                };
-            });
-
-        // add a park source
-        outputArray["sources"].unshift({
-            index: -1,
-            label: "Park",
-            fixed: true,
-            selected: selectedSourceIndex === -1,
-            order: -1,
-            icon: "mdi-car-brake-parking",
-            iconColor: null,
+    outputArray["sources"] = inputLabels
+        .filter((labelItem, index) => validSources.includes(index))
+        .map((labelItem) => {
+            const labelIndex = labelItem[0];
+            return {
+                index: labelIndex,
+                label: labelItem[1],
+                fixed: buttonsFixed,
+                selected: selectedSourceIndex === labelIndex,
+                order: groupIndex !== null ? validSources.indexOf(labelIndex) : labelIndex,
+                icon: icons[labelIndex] ? icons[labelIndex] : null,
+                iconColor: iconColors[labelIndex] ? iconColors[labelIndex] : "#ffffff",
+            };
         });
-        // sort by order field
-        outputArray["sources"].sort((a, b) => (a.order > b.order ? 1 : -1));
-    }
+
+    // add a park source
+    outputArray["sources"].unshift({
+        index: -1,
+        label: "Park",
+        fixed: true,
+        selected: selectedSourceIndex === -1,
+        order: -1,
+        icon: "mdi-car-brake-parking",
+        iconColor: null,
+    });
+    // sort by order field
+    outputArray["sources"].sort((a, b) => (a.order > b.order ? 1 : -1));
+
     return outputArray;
 };
