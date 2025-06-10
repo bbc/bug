bd219346dc08bb91d748f5ff1b7e4b426aba2e2b
Geoff House <geoff.house@bbc.co.uk>
2025-06-10 07:50:14 +0100

    netgear-avline: fix missing vlans

--- src/modules/netgear-avline/container/utils/poller-interfacevlans.js
+++ src/modules/netgear-avline/container/utils/poller-interfacevlans.js
@@ -6,43 +6,45 @@ const delay = require("delay");
 module.exports = async (NetgearApi, interfacesCollection) => {
 
     const vlans = await mongoSingle.get("vlans");
-    const availableVlans = vlans.map((v) => v.id);
+    if (vlans && vlans.length > 0) {
+        const availableVlans = vlans.map((v) => v.id);
 
-    // get list of interfaces
-    const interfaces = await interfacesCollection.find().toArray();
+        // get list of interfaces
+        const interfaces = await interfacesCollection.find().toArray();
 
-    for (let eachInterface of interfaces) {
-        const fieldsToUpdate = {};
+        for (let eachInterface of interfaces) {
+            const fieldsToUpdate = {};
 
-        const result = await NetgearApi.get({ path: `dot1q_sw_port_config?interface=${eachInterface.port}` });
-        if (result?.dot1q_sw_port_config) {
+            const result = await NetgearApi.get({ path: `dot1q_sw_port_config?interface=${eachInterface.port}` });
+            if (result?.dot1q_sw_port_config) {
 
-            fieldsToUpdate["configMode"] = result.dot1q_sw_port_config.configMode
+                fieldsToUpdate["configMode"] = result.dot1q_sw_port_config.configMode
 
-            if (result.dot1q_sw_port_config.configMode === "access") {
-                fieldsToUpdate["untagged-vlan"] = result.dot1q_sw_port_config?.accessVlan;
-                fieldsToUpdate["tagged-vlans"] = [];
+                if (result.dot1q_sw_port_config.configMode === "access") {
+                    fieldsToUpdate["untagged-vlan"] = result.dot1q_sw_port_config?.accessVlan;
+                    fieldsToUpdate["tagged-vlans"] = [];
+                }
+                else if (result.dot1q_sw_port_config.configMode === "trunk") {
+                    fieldsToUpdate["untagged-vlan"] = result.dot1q_sw_port_config?.nativeVlan;
+                    fieldsToUpdate["tagged-vlans"] = result.dot1q_sw_port_config?.allowedVlanList?.filter((v) => availableVlans.includes(parseInt(v))).map((v) => parseInt(v));
+                }
+                else if (result.dot1q_sw_port_config.configMode === "general") {
+                    fieldsToUpdate["untagged-vlan"] = result.dot1q_sw_port_config?.nativeVlan;
+                    fieldsToUpdate["tagged-vlans"] = result.dot1q_sw_port_config?.allowedVlanList?.filter((v) => availableVlans.includes(parseInt(v))).map((v) => parseInt(v));
+                }
+
             }
-            else if (result.dot1q_sw_port_config.configMode === "trunk") {
-                fieldsToUpdate["untagged-vlan"] = result.dot1q_sw_port_config?.nativeVlan;
-                fieldsToUpdate["tagged-vlans"] = result.dot1q_sw_port_config?.allowedVlanList?.filter((v) => availableVlans.includes(parseInt(v))).map((v) => parseInt(v));
-            }
-            else if (result.dot1q_sw_port_config.configMode === "general") {
-                fieldsToUpdate["untagged-vlan"] = result.dot1q_sw_port_config?.nativeVlan;
-                fieldsToUpdate["tagged-vlans"] = result.dot1q_sw_port_config?.allowedVlanList?.filter((v) => availableVlans.includes(parseInt(v))).map((v) => parseInt(v));
-            }
+
+            // save to DB
+            await interfacesCollection.updateOne(
+                {
+                    interfaceId: eachInterface.interfaceId,
+                },
+                { $set: fieldsToUpdate }
+            );
+
+            await delay(5000);
 
         }
-
-        // save to DB
-        await interfacesCollection.updateOne(
-            {
-                interfaceId: eachInterface.interfaceId,
-            },
-            { $set: fieldsToUpdate }
-        );
-
-        await delay(5000);
-
     }
 }
