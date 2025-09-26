a6f6860c6364465de00e295e1e1c51e5988c8a32
Geoff House <geoff.house@bbc.co.uk>
2025-09-26 17:28:32 +0100

    ross-ultix: add ability to limit groups

--- src/modules/ross-ultrix/container/services/source-list.js
+++ src/modules/ross-ultrix/container/services/source-list.js
@@ -24,8 +24,23 @@ module.exports = async (destinationIndex = null, groupIndex = 0, showExcluded = 
     const groups = await sourceGroupList();
     const routesCollection = await mongoCollection("routes");
 
+    // check limited groups
+    const limitSourceGroups = config.limitSourceGroups.sort((a, b) => {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+
+    let filteredGroups;
+    if (limitSourceGroups.length > 0) {
+        filteredGroups = groups.filter((g) =>
+            limitSourceGroups.some((label) => label.toLowerCase() === g.label.toLowerCase())
+        );
+    }
+    else {
+        filteredGroups = groups;
+    }
+
     const outputArray = {
-        groups: groups.map((g) => {
+        groups: filteredGroups.map((g) => {
             return {
                 label: g.label,
                 index: g.index,
@@ -37,43 +52,47 @@ module.exports = async (destinationIndex = null, groupIndex = 0, showExcluded = 
         }),
         sources: [],
     };
+    // check that selected group is in the filteredGroups
+    if (filteredGroups.some(
+        (l) => l.label.toLowerCase() === groups[groupIndex].label.toLowerCase()
+    )) {
 
-    // calculate excluded sources
-    // not that this field is an array of strings - so we call toString() on each check later on. Grrrrr.
-    const excludedSources = config["excludeSources"] ? config["excludeSources"] : [];
+        // calculate excluded sources
+        // not that this field is an array of strings - so we call toString() on each check later on. Grrrrr.
+        const excludedSources = config["excludeSources"] ? config["excludeSources"] : [];
 
-    const buttonsFixed = groups[groupIndex]?.fixed ?? false;
+        const buttonsFixed = groups[groupIndex]?.fixed ?? false;
 
-    // get get existing routes from the db
-    const crosspoints = await routesCollection.findOne({ destination: parseInt(destinationIndex) });
+        // get get existing routes from the db
+        const crosspoints = await routesCollection.findOne({ destination: parseInt(destinationIndex) });
 
-    let selectedSources = [];
-    if (crosspoints?.levels) {
-        selectedSources = Object.values(crosspoints?.levels) ?? [];
-    }
-
-    groups[groupIndex]?.["value"].forEach((sourceIndex, order) => {
-
-        // check it's not excluded or if it's a selected source - in which case we'll show it anyway
-        const isExcluded = excludedSources.includes(sourceIndex?.toString());
-        const isSelected = selectedSources.includes(sourceIndex);
-        if (!isExcluded || showExcluded) {
-            outputArray["sources"].push({
-                index: sourceIndex,
-                label: sources[sourceIndex]?.name,
-                fixed: buttonsFixed,
-                description: sources[sourceIndex]?.description,
-                selected: isSelected,
-                hidden: isExcluded,
-                order: order,
-                icon: icons[sourceIndex] ? icons[sourceIndex] : null,
-                iconColor: iconColors[sourceIndex] ? iconColors[sourceIndex] : "#ffffff",
-            });
+        let selectedSources = [];
+        if (crosspoints?.levels) {
+            selectedSources = Object.values(crosspoints?.levels) ?? [];
         }
-    })
 
-    // sort by order field
-    outputArray["sources"].sort((a, b) => (a.order > b.order ? 1 : -1));
+        groups[groupIndex]?.["value"].forEach((sourceIndex, order) => {
 
+            // check it's not excluded or if it's a selected source - in which case we'll show it anyway
+            const isExcluded = excludedSources.includes(sourceIndex?.toString());
+            const isSelected = selectedSources.includes(sourceIndex);
+            if (!isExcluded || showExcluded) {
+                outputArray["sources"].push({
+                    index: sourceIndex,
+                    label: sources[sourceIndex]?.name,
+                    fixed: buttonsFixed,
+                    description: sources[sourceIndex]?.description,
+                    selected: isSelected,
+                    hidden: isExcluded,
+                    order: order,
+                    icon: icons[sourceIndex] ? icons[sourceIndex] : null,
+                    iconColor: iconColors[sourceIndex] ? iconColors[sourceIndex] : "#ffffff",
+                });
+            }
+        })
+
+        // sort by order field
+        outputArray["sources"].sort((a, b) => (a.order > b.order ? 1 : -1));
+    }
     return outputArray;
 };
