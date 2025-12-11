8840f89f2bd59eb02d0b1ae867e482f855f19a5b
Geoff House <geoff.house@bbc.co.uk>
2025-12-11 14:00:45 +0000

    fix menu buttons

--- src/client/src/components/Menu.jsx
+++ src/client/src/components/Menu.jsx
@@ -12,7 +12,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import FaviconNotification from "@utils/FaviconNotification";
@@ -37,7 +37,6 @@ const Menu = ({ showGroups = true }) => {
     const enabledStrategiesCount = strategies.data.filter((eachStrategy) => eachStrategy.enabled).length;
 
     const getSelectedGroup = () => {
-        // this is used to expand the groups when the page is loaded with a panel already open
         let selectedGroup = null;
         if (panelList.status === "success" && panelList.data && panel.status === "success" && panel.data) {
             for (let eachPanel of panelList.data) {
@@ -49,31 +48,29 @@ const Menu = ({ showGroups = true }) => {
         return selectedGroup;
     };
 
-    // we fetch this here so we can use it as a dependency in the useEffect
     const selectedGroup = getSelectedGroup();
 
     useEffect(() => {
-        // only run this if the selectedGroup changes
         if (selectedGroup) {
             setExpanded(selectedGroup);
         }
     }, [selectedGroup]);
 
     const renderMenuItem = (menuPanel) => {
-        if (!menuPanel.enabled) {
-            return null;
-        }
+        if (!menuPanel.enabled) return null;
+
         const isSelected = panel.status === "success" && menuPanel.id === panel.data.id;
+
         return (
             <BugRestrictTo key={menuPanel.id} panel={menuPanel?.id}>
-                <ListItem button component={Link} to={`/panel/${menuPanel.id}`} selected={isSelected}>
-                    <ListItemIcon>
+                <ListItemButton component={Link} to={`/panel/${menuPanel.id}`} selected={isSelected}>
+                    <ListItemIcon sx={{ color: "inherit" }}>
                         <BadgeWrapper panel={menuPanel}>
                             <BugDynamicIcon iconName={menuPanel._module.icon} />
                         </BadgeWrapper>
                     </ListItemIcon>
                     <ListItemText primary={menuPanel.title} />
-                </ListItem>
+                </ListItemButton>
             </BugRestrictTo>
         );
     };
@@ -89,12 +86,8 @@ const Menu = ({ showGroups = true }) => {
                     key={groupArrayItem.group}
                     elevation={0}
                     sx={{
-                        "&.MuiAccordion-root:before": {
-                            height: "0px",
-                        },
-                        "&.Mui-expanded": {
-                            margin: "0px",
-                        },
+                        "&.MuiAccordion-root:before": { height: "0px" },
+                        "&.Mui-expanded": { margin: "0px" },
                     }}
                     expanded={expanded === groupArrayItem.group}
                     onChange={handleAccordionChange(groupArrayItem.group)}
@@ -105,37 +98,18 @@ const Menu = ({ showGroups = true }) => {
                             fontWeight: 500,
                             textTransform: "uppercase",
                             height: "48px",
-                            "&.Mui-expanded": {
-                                minHeight: "48px",
-                                height: "48px",
-                            },
+                            "&.Mui-expanded": { minHeight: "48px", height: "48px" },
                             color: "primary.main",
-                            "& .MuiAccordionSummary-content": {
-                                order: 2,
-                            },
-                            "& .MuiButtonBase-root": {
-                                paddingLeft: "0px",
-                                marginRight: "0px",
-                            },
-                            "& .MuiAccordionSummary-expandIcon.Mui-expanded": {
-                                transform: "none",
-                            },
+                            "& .MuiAccordionSummary-content": { order: 2 },
+                            "& .MuiButtonBase-root": { paddingLeft: 0, marginRight: 0 },
+                            "& .MuiAccordionSummary-expandIcon.Mui-expanded": { transform: "none" },
                         }}
                         expandIcon={expanded === groupArrayItem.group ? <ArrowDropDownIcon /> : <ArrowRightIcon />}
                     >
                         {groupArrayItem.group}
                     </AccordionSummary>
-                    <AccordionDetails
-                        sx={{
-                            padding: "0px",
-                            display: "block",
-                        }}
-                    >
-                        <List
-                            sx={{
-                                padding: "0px",
-                            }}
-                        >
+                    <AccordionDetails sx={{ padding: 0, display: "block" }}>
+                        <List sx={{ padding: 0 }}>
                             {groupArrayItem.items.map((eachPanel) => renderMenuItem(eachPanel))}
                         </List>
                     </AccordionDetails>
@@ -151,9 +125,7 @@ const Menu = ({ showGroups = true }) => {
     };
 
     const renderPanelMenuItems = (roles = []) => {
-        if (panelList.status === "loading") {
-            return <BugLoading />;
-        }
+        if (panelList.status === "loading") return <BugLoading />;
         if (panelList.status === "success") {
             if (roles.includes("user") || enabledStrategiesCount === 0) {
                 const panelsByGroup = panelListGroups(
@@ -164,33 +136,23 @@ const Menu = ({ showGroups = true }) => {
                 );
                 return panelsByGroup.map((groups) => groupedMenuItems(groups));
             }
-        } else {
-            return null;
         }
+        return null;
     };
 
     const setNotifications = (panels) => {
-        const notificationCount = {
-            info: 0,
-            warning: 0,
-            critical: 0,
-            error: 0,
-        };
-
+        const notificationCount = { info: 0, warning: 0, critical: 0, error: 0 };
         for (let panel of panels) {
-            const criticalStatusCount = panel._status && panel._status.filter((x) => x.type === "critical").length;
-
-            // if we have any critical statuses for this panel, ignore any others
+            const criticalStatusCount = panel._status?.filter((x) => x.type === "critical").length || 0;
             if (criticalStatusCount > 0) {
-                notificationCount["critical"] += 1;
+                notificationCount.critical += 1;
             } else {
-                // otherwise, count all the statuses
-                for (let notification of panel._status) {
+                for (let notification of panel._status || []) {
                     notificationCount[notification.type] += 1;
                 }
             }
         }
-        if (settings?.data?.title && settings?.data?.title !== "") {
+        if (settings?.data?.title) {
             faviconNotification.setOptions({ title: settings.data.title });
         }
         faviconNotification.set(notificationCount);
@@ -198,84 +160,72 @@ const Menu = ({ showGroups = true }) => {
 
     setNotifications(activePanelList);
 
-    //TODO move enabledStrategiesCount into redux user slice
     return (
-        <>
-            <Grid
-                container
-                sx={{
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                    alignItems: "flex-start",
-                    height: "100%",
-                }}
-            >
-                <Grid item style={{ width: "100%" }}>
-                    {(user?.data || enabledStrategiesCount === 0) && (
-                        <>
-                            <List disablePadding>
-                                <ListItem
-                                    button
-                                    component={Link}
-                                    to="/"
-                                    selected={location.pathname === "/"}
-                                    onClick={() => {
-                                        setExpanded(false);
-                                    }}
-                                >
-                                    <ListItemIcon>
-                                        <BugDynamicIcon iconName={"Home"} />
-                                    </ListItemIcon>
-                                    <ListItemText primary="Home" />
-                                </ListItem>
-                            </List>
-                            <MenuDivider />
-                            {renderPanelMenuItems(user?.data?.roles, user?.data?.panels)}
-                            {activePanelList.length > 0 ? <MenuDivider /> : null}
+        <Grid
+            container
+            sx={{
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "flex-start",
+                height: "100%",
+            }}
+        >
+            <Grid item style={{ width: "100%" }}>
+                {(user?.data || enabledStrategiesCount === 0) && (
+                    <>
+                        <List disablePadding>
+                            <ListItemButton
+                                component={Link}
+                                to="/"
+                                selected={location.pathname === "/"}
+                                onClick={() => setExpanded(false)}
+                            >
+                                <ListItemIcon sx={{ color: "inherit" }}>
+                                    <BugDynamicIcon iconName="Home" />
+                                </ListItemIcon>
+                                <ListItemText primary="Home" />
+                            </ListItemButton>
+                        </List>
+                        <MenuDivider />
+                        {renderPanelMenuItems(user?.data?.roles, user?.data?.panels)}
+                        {activePanelList.length > 0 ? <MenuDivider /> : null}
 
-                            <BugRestrictTo role="admin">
-                                <List disablePadding>
-                                    <ListItem
-                                        button
-                                        component={Link}
-                                        to="/system"
-                                        selected={location.pathname.startsWith("/system")}
-                                        onClick={() => {
-                                            setExpanded(false);
-                                        }}
-                                    >
-                                        <ListItemIcon>
-                                            <BugDynamicIcon iconName={"Settings"} />
-                                        </ListItemIcon>
-                                        <ListItemText primary="System" />
-                                    </ListItem>
-                                    <ListItem
-                                        button
-                                        component={Link}
-                                        to="/panels"
-                                        selected={location.pathname.startsWith("/panels")}
-                                        onClick={() => {
-                                            setExpanded(false);
-                                        }}
-                                    >
-                                        <ListItemIcon>
-                                            <BugDynamicIcon iconName={"Dashboard"} />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Panels" />
-                                    </ListItem>
-                                </List>
-                            </BugRestrictTo>
-                        </>
-                    )}
-                </Grid>
-                <Grid item style={{ width: "100%" }}>
-                    <List>
-                        <UserMenuItem />
-                        <BugMenuIcon />
-                    </List>
-                </Grid>
+                        <BugRestrictTo role="admin">
+                            <List disablePadding>
+                                <ListItemButton
+                                    component={Link}
+                                    to="/system"
+                                    selected={location.pathname.startsWith("/system")}
+                                    onClick={() => setExpanded(false)}
+                                >
+                                    <ListItemIcon sx={{ color: "inherit" }}>
+                                        <BugDynamicIcon iconName="Settings" />
+                                    </ListItemIcon>
+                                    <ListItemText primary="System" />
+                                </ListItemButton>
+                                <ListItemButton
+                                    component={Link}
+                                    to="/panels"
+                                    selected={location.pathname.startsWith("/panels")}
+                                    onClick={() => setExpanded(false)}
+                                >
+                                    <ListItemIcon sx={{ color: "inherit" }}>
+                                        <BugDynamicIcon iconName="Dashboard" />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Panels" />
+                                </ListItemButton>
+                            </List>
+                        </BugRestrictTo>
+                    </>
+                )}
             </Grid>
-        </>
+            <Grid item style={{ width: "100%" }}>
+                <List>
+                    <UserMenuItem />
+                    <BugMenuIcon />
+                </List>
+            </Grid>
+        </Grid>
     );
 };
 
