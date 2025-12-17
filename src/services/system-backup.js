ed316664c571495d2710d7250a84302b26b6c93c
Geoff House <geoff.house@bbc.co.uk>
2025-12-17 08:12:51 +0000

    remove moment.js

--- src/services/system-backup.js
+++ src/services/system-backup.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const path = require("path");
+const { format } = require("date-fns");
 const logger = require("@utils/logger")(module);
 const tarFolder = require("@utils/tar-folder");
 const getSystemSettings = require("@services/system-settings-get");
-const moment = require("moment");
 
 module.exports = async () => {
     try {
@@ -12,7 +12,7 @@ module.exports = async () => {
         let title = systemSettings.data?.title;
 
         if (title) {
-            //Santize the title so it can be used as a filename
+            // Sanitize the title so it can be used as a filename
             title = title.toLowerCase();
             title = title.replace(/[/\\?%*:|"<>]/g, "_");
             title = title.replace(" ", "-");
@@ -21,12 +21,12 @@ module.exports = async () => {
         }
 
         const configFolder = path.join(__dirname, "..", "config");
-        const filename = `${title}-backup-${moment().format("DD-MM-YYYY-HH-mm-ss")}.tgz`;
+        const filename = `${title}-backup-${format(new Date(), "dd-MM-yyyy-HH-mm-ss")}.tgz`;
         const stream = await tarFolder(configFolder, ["panels", "global"]);
 
         return {
-            stream: stream,
-            filename: filename,
+            stream,
+            filename,
         };
     } catch (error) {
         logger.warning(`${error.stack || error.trace || error || error.message}`);
