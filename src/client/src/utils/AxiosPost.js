64e8ea158fa66be1e1652770a408bb80055bd475
Geoff House <geoff.house@bbc.co.uk>
2026-01-27 07:43:38 +0000

    add try/catch to Axios helpers

--- src/client/src/utils/AxiosPost.js
+++ src/client/src/utils/AxiosPost.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
 export default async function AxiosPost(url, data) {
-    let response = await axios.post(url, data);
-    switch (response.data.status) {
-        case "success":
-            response = response.data.data;
-            break;
-        case "failure":
-            console.error("failed to fetch " + url);
-            response = null;
-            break;
-        case "error":
-            console.error(response.data.message);
-            response = null;
-            break;
-        default:
-            response = null;
+    try {
+        let response = await axios.post(url, data);
+        switch (response.data.status) {
+            case "success":
+                response = response.data.data;
+                break;
+            case "failure":
+                console.error("failed to fetch " + url);
+                response = null;
+                break;
+            case "error":
+                console.error(response.data.message);
+                response = null;
+                break;
+            default:
+                response = null;
+        }
+        return response;
+    } catch (error) {
+        console.log("ERROR", error);
+        return false;
     }
-    return response;
 }
--- src/client/src/utils/AxiosPut.js
+++ src/client/src/utils/AxiosPut.js
@@ -1,22 +1,27 @@
 import axios from "axios";
 
 export default async function AxiosPut(url, data) {
-    let response = await axios.put(url, data);
+    try {
+        let response = await axios.put(url, data);
 
-    switch (response?.data?.status) {
-        case "success":
-            response = response?.data?.data;
-            break;
-        case "failure":
-            console.error("failed to PUT " + url);
-            response = null;
-            break;
-        case "error":
-            console.error(response?.data?.message);
-            response = null;
-            break;
-        default:
-            response = null;
+        switch (response?.data?.status) {
+            case "success":
+                response = response?.data?.data;
+                break;
+            case "failure":
+                console.error("failed to PUT " + url);
+                response = null;
+                break;
+            case "error":
+                console.error(response?.data?.message);
+                response = null;
+                break;
+            default:
+                response = null;
+        }
+        return response;
+    } catch (error) {
+        console.log("ERROR", error);
+        return false;
     }
-    return response;
 }
