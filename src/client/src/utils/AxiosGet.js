cbbf343955f05d540c51ab13f480a8059ec5be7f
Geoff House <geoff.house@bbc.co.uk>
2026-01-27 07:43:38 +0000

    add try/catch to Axios helpers

--- src/client/src/utils/AxiosGet.js
+++ src/client/src/utils/AxiosGet.js
@@ -1,18 +1,23 @@
 import axios from "axios";
 
 export default async function AxiosGet(url) {
-    const response = await axios.get(url);
+    try {
+        const response = await axios.get(url);
 
-    switch (response.data.status) {
-        case "success":
-            return response.data.data;
-        case "failure":
-            console.error("failed to fetch " + url);
-            return null;
-        case "error":
-            console.error(response.data.message);
-            return null;
-        default:
-            return null;
+        switch (response.data.status) {
+            case "success":
+                return response.data.data;
+            case "failure":
+                console.error("failed to fetch " + url);
+                return null;
+            case "error":
+                console.error(response.data.message);
+                return null;
+            default:
+                return null;
+        }
+    } catch (error) {
+        console.log("ERROR", error);
+        return false;
     }
 }
