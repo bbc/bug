ce00615c8431b0b33c715fb0a13390e2d5692754
Geoff House <geoff.house@bbc.co.uk>
2026-02-04 09:15:11 +0000

    [bmd-multiiew] update routes to use asynchandler

--- src/modules/bmd-multiview16/container/api/routes/destination.js
+++ src/modules/bmd-multiview16/container/api/routes/destination.js
@@ -1,34 +1,20 @@
 const express = require("express");
 const destinationRoute = require("@services/destination-route");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
 
-router.get("/setaudio/:sourceIndex", async function (req, res, next) {
-    try {
-        res.json({
-            status: "success",
-            data: await destinationRoute(17, req?.params?.sourceIndex),
-        });
-    } catch (error) {
-        console.log(error);
-        res.json({
-            status: "error",
-            message: "Failed to set audio solo",
-        });
-    }
-});
+router.get("/setaudio/:sourceIndex", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await destinationRoute(17, req?.params?.sourceIndex),
+    });
+}));
 
-router.get("/:destinationIndex/:sourceIndex", async function (req, res, next) {
-    try {
-        res.json({
-            status: "success",
-            data: await destinationRoute(req.params?.destinationIndex, req.params?.sourceIndex),
-        });
-    } catch (error) {
-        res.json({
-            status: "error",
-            message: "Failed to route",
-        });
-    }
-});
+router.get("/:destinationIndex/:sourceIndex", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await destinationRoute(req.params?.destinationIndex, req.params?.sourceIndex),
+    });
+}));
 
 module.exports = router;
--- src/modules/bmd-multiview16/container/api/routes/source.js
+++ src/modules/bmd-multiview16/container/api/routes/source.js
@@ -3,50 +3,27 @@ const sourceList = require("@services/source-list");
 const router = express.Router();
 const sourceSetSolo = require("@services/source-setsolo");
 const sourceClearSolo = require("@services/source-clearsolo");
+const asyncHandler = require("express-async-handler");
 
-router.get("/", async function (req, res, next) {
-    try {
-        res.json({
-            status: "success",
-            data: await sourceList(),
-        });
-    } catch (error) {
-        console.log(error);
-        res.json({
-            status: "error",
-            message: "Failed to get source list",
-        });
-    }
-});
+router.get("/", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await sourceList(),
+    });
+}));
 
-router.get("/setsolo/:sourceIndex", async function (req, res, next) {
-    try {
-        res.json({
-            status: "success",
-            data: await sourceSetSolo(req?.params?.sourceIndex),
-        });
-    } catch (error) {
-        console.log(error);
-        res.json({
-            status: "error",
-            message: "Failed to set source solo",
-        });
-    }
-});
+router.get("/setsolo/:sourceIndex", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await sourceSetSolo(req?.params?.sourceIndex),
+    });
+}));
 
-router.get("/clearsolo", async function (req, res, next) {
-    try {
-        res.json({
-            status: "success",
-            data: await sourceClearSolo(),
-        });
-    } catch (error) {
-        console.log(error);
-        res.json({
-            status: "error",
-            message: "Failed to clear source solo",
-        });
-    }
-});
+router.get("/clearsolo", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await sourceClearSolo(),
+    });
+}));
 
 module.exports = router;
