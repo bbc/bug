d541d377b3c2d0af0d8c3dc19cf9309c0a3fbd95
Geoff House <geoff.house@bbc.co.uk>
2026-02-04 14:39:04 +0000

    [cisco-cbs] improve logging in status handlers

--- src/modules/cisco-cbs/container/services/status-checkpending.js
+++ src/modules/cisco-cbs/container/services/status-checkpending.js
@@ -4,15 +4,20 @@ const StatusItem = require("@core/StatusItem");
 const mongoSingle = require("@core/mongo-single");
 
 module.exports = async () => {
-    const pending = await mongoSingle.get("pending");
-    if (pending) {
-        return [
-            new StatusItem({
-                key: `pending`,
-                message: [`Device has unsaved changes`],
-                type: "warning",
-            }),
-        ];
+    try {
+        const pending = await mongoSingle.get("pending");
+        if (pending) {
+            return [
+                new StatusItem({
+                    key: `pending`,
+                    message: [`Device has unsaved changes`],
+                    type: "warning",
+                }),
+            ];
+        }
+        return [];
+    } catch (error) {
+        console.error(`status-checkpending: ${error.message}`);
+        throw error;
     }
-    return [];
 };
