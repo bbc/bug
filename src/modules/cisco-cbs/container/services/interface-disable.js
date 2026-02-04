471d8eeb1c57a008bc74852913c992d8adb65445
Geoff House <geoff.house@bbc.co.uk>
2026-02-04 14:37:14 +0000

    [cisco-cbs] refactor services with try/catch/throw

--- src/modules/cisco-cbs/container/services/interface-disable.js
+++ src/modules/cisco-cbs/container/services/interface-disable.js
@@ -5,40 +5,53 @@ const configGet = require("@core/config-get");
 const mongoCollection = require("@core/mongo-collection");
 
 module.exports = async (interfaceId) => {
-    const config = await configGet();
+    try {
+        console.log(`interface-disable: start - interfaceId=${interfaceId}`);
 
-    // create new snmp session
-    const snmpAwait = new SnmpAwait({
-        host: config.address,
-        community: config.snmpCommunity,
-    });
-
-    console.log(`interface-disable: disabling interface ${interfaceId} ...`);
-
-    const result = await snmpAwait.set({
-        oid: `1.3.6.1.2.1.2.2.1.7.${interfaceId}`,
-        value: 2,
-    });
-
-    // we're done with the SNMP session
-    snmpAwait.close();
-
-    if (result) {
-        console.log(`interface-disable: success - updating DB`);
-        try {
-            const interfacesCollection = await mongoCollection("interfaces");
-            const dbResult = await interfacesCollection.updateOne(
-                { interfaceId: parseInt(interfaceId) },
-                { $set: { "admin-state": false } }
-            );
-            console.log(`interface-disable: ${JSON.stringify(dbResult.result)}`);
-            return true;
-        } catch (error) {
-            console.log(`interface-disable: failed to update db`);
-            console.log(error);
-            return false;
+        // validate input
+        if (!interfaceId) {
+            throw new Error("invalid input: interfaceId is required");
         }
+
+        const config = await configGet();
+        if (!config || !config.address || !config.snmpCommunity) {
+            throw new Error("failed to load config");
+        }
+
+        // create new snmp session
+        const snmpAwait = new SnmpAwait({
+            host: config.address,
+            community: config.snmpCommunity,
+        });
+
+        console.log(`interface-disable: disabling interface ${interfaceId} via SNMP`);
+
+        const result = await snmpAwait.set({
+            oid: `1.3.6.1.2.1.2.2.1.7.${interfaceId}`,
+            value: 2,
+        });
+
+        // we're done with the SNMP session
+        snmpAwait.close();
+
+        if (!result) {
+            throw new Error(`failed to enable interface ${interfaceId}`);
+        }
+
+        console.log(`interface-disable: snmp success - updating DB`);
+
+        const interfacesCollection = await mongoCollection("interfaces");
+
+        const dbResult = await interfacesCollection.updateOne(
+            { interfaceId: parseInt(interfaceId) },
+            { $set: { "admin-state": false } }
+        );
+
+        console.log(`interface-disable: db update result - ${JSON.stringify(dbResult)}`);
+        return true;
+
+    } catch (err) {
+        err.message = `interface-disable: ${err.stack || err.message || err}`;
+        throw err;
     }
-    console.log(`interface-disable: failed to disable interface ${interfaceId}`);
-    return false;
 };
--- src/modules/cisco-cbs/container/services/interface-rename.js
+++ src/modules/cisco-cbs/container/services/interface-rename.js
@@ -5,40 +5,50 @@ const configGet = require("@core/config-get");
 const mongoCollection = require("@core/mongo-collection");
 
 module.exports = async (interfaceId, newName) => {
-    const config = await configGet();
-
-    // create new snmp session
-    const snmpAwait = new SnmpAwait({
-        host: config.address,
-        community: config.snmpCommunity,
-    });
-
-    console.log(`interface-rename: renaming interface ${interfaceId} to ${newName} ...`);
-
-    const result = await snmpAwait.set({
-        oid: `.1.3.6.1.2.1.31.1.1.1.18.${interfaceId}`,
-        value: newName.toString(),
-    });
-
-    // we're done with the SNMP session
-    snmpAwait.close();
-
-    if (result) {
-        console.log(`interface-rename: success - updating DB`);
-        try {
-            const interfacesCollection = await mongoCollection("interfaces");
-            const dbResult = await interfacesCollection.updateOne(
-                { interfaceId: parseInt(interfaceId) },
-                { $set: { alias: newName } }
-            );
-            console.log(`interface-rename: ${JSON.stringify(dbResult.result)}`);
-            return true;
-        } catch (error) {
-            console.log(`interface-rename: failed to update db`);
-            console.log(error);
-            return false;
+    try {
+        const config = await configGet();
+        if (!config) {
+            throw new Error("failed to load config");
         }
+
+        if (!interfaceId || newName === undefined || newName === null) {
+            throw new Error("invalid input");
+        }
+
+        // create new snmp session
+        const snmpAwait = new SnmpAwait({
+            host: config.address,
+            community: config.snmpCommunity,
+        });
+
+        console.log(`interface-rename: renaming interface ${interfaceId} to ${newName} ...`);
+
+        const result = await snmpAwait.set({
+            oid: `.1.3.6.1.2.1.31.1.1.1.18.${interfaceId}`,
+            value: newName.toString(),
+        });
+
+        // we're done with the SNMP session
+        snmpAwait.close();
+
+        if (!result) {
+            console.log(`interface-rename: failed to rename interface ${interfaceId} to ${newName}`);
+            throw new Error("snmp set failed");
+        }
+
+        console.log(`interface-rename: success - updating DB`);
+
+        const interfacesCollection = await mongoCollection("interfaces");
+        const dbResult = await interfacesCollection.updateOne(
+            { interfaceId: parseInt(interfaceId) },
+            { $set: { alias: newName } }
+        );
+
+        console.log(`interface-rename: ${JSON.stringify(dbResult.result)}`);
+
+        return true;
+    } catch (err) {
+        err.message = `interface-rename: ${err.stack || err.message || err}`;
+        throw err;
     }
-    console.log(`interface-rename: failed to rename interface ${interfaceId} to ${newName}`);
-    return false;
 };
