7522afc5e3608741043f5ce72b8ac5c8dddafd2b
Geoff House <geoff.house@bbc.co.uk>
2026-02-04 12:29:16 +0000

    [cisco-cbs] improve api route error handling

--- src/modules/cisco-cbs/container/api/routes/device.js
+++ src/modules/cisco-cbs/container/api/routes/device.js
@@ -4,28 +4,19 @@ const deviceSave = require("@services/device-save");
 const deviceStackCount = require("@services/device-stackcount");
 const asyncHandler = require("express-async-handler");
 
-router.get("/save", async function (req, res, next) {
-    try {
-        res.json({
-            status: "success",
-            data: await deviceSave(),
-        });
-    } catch (error) {
-        console.log(error);
-        res.json({
-            status: "error",
-            message: "Failed to save device config",
-        });
-    }
-});
+router.get("/save", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await deviceSave(),
+    });
+}));
 
 router.get(
     "/stackcount",
     asyncHandler(async (req, res) => {
-        const result = await deviceStackCount();
         res.json({
-            status: result ? "success" : "failure",
-            data: result,
+            status: "success",
+            data: await deviceStackCount(),
         });
     })
 );
