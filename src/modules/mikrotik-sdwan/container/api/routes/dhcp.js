6037eef099255539145e2561c1dc67fcd8d8ea27
Geoff House <geoff.house@bbc.co.uk>
2026-02-09 07:06:07 +0000

    use asynchandler for routes

--- src/modules/mikrotik-sdwan/container/api/routes/dhcp.js
+++ src/modules/mikrotik-sdwan/container/api/routes/dhcp.js
@@ -6,22 +6,23 @@ const dhcpLeaseDelete = require("@services/dhcplease-delete");
 const dhcpNetworkList = require("@services/dhcpnetwork-list");
 const dhcpServerList = require("@services/dhcpserver-list");
 const entryDeleteRoute = require("@services/entry-deleteroute");
+const asyncHandler = require("express-async-handler");
 
-const catchAsync = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-router.post("/lease", catchAsync(async (req, res) => {
-    const data = await dhcpLeaseList(req.body.sortField, req.body.sortDirection, req.body.filters);
-    res.json({ status: "success", data });
+router.post("/lease", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await dhcpLeaseList(req.body.sortField, req.body.sortDirection, req.body.filters)
+    });
 }));
 
-router.post("/add", catchAsync(async (req, res) => {
-    const data = await dhcpLeaseAdd(req.body);
-    res.json({ status: "success", data });
+router.post("/add", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await dhcpLeaseAdd(req.body)
+    });
 }));
 
-router.delete("/:address", catchAsync(async (req, res) => {
+router.delete("/:address", asyncHandler(async (req, res) => {
     // delete the lease first
     const data = await dhcpLeaseDelete(req.params.address);
 
@@ -35,14 +36,18 @@ router.delete("/:address", catchAsync(async (req, res) => {
     res.json({ status: "success", data });
 }));
 
-router.get("/network", catchAsync(async (req, res) => {
-    const data = await dhcpNetworkList();
-    res.json({ status: "success", data });
+router.get("/network", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await dhcpNetworkList()
+    });
 }));
 
-router.get("/server", catchAsync(async (req, res) => {
-    const data = await dhcpServerList();
-    res.json({ status: "success", data });
+router.get("/server", asyncHandler(async (req, res) => {
+    res.json({
+        status: "success",
+        data: await dhcpServerList()
+    });
 }));
 
 module.exports = router;
