c54364affe6ca9e1be96634497e77e17ddfb24f1
Geoff House <geoff.house@bbc.co.uk>
2026-02-10 08:13:24 +0000

    remove densitron-emu3 module

--- src/modules/densitron-emu3/container/services/output-list.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const deviceGet = require("./device-get");
-
-module.exports = async (deviceIndex) => {
-    const device = await deviceGet(deviceIndex);
-    return device.outputs;
-};
