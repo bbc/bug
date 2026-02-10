f09ab0407e8e52005e6d39b4b279f35874a46060
Geoff House <geoff.house@bbc.co.uk>
2026-02-10 08:15:51 +0000

    remove fscom-s3410 module

--- src/modules/fscom-s3410/container/services/interface-get.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const mongoCollection = require("@core/mongo-collection");
-
-module.exports = async (interfaceId) => {
-    const dbInterfaces = await mongoCollection("interfaces");
-    return await dbInterfaces.findOne({ interfaceId: parseInt(interfaceId) });
-};
