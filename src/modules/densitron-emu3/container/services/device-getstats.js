03ae1e76d22682faaa37869dfa45ab1aaba3d94e
Geoff House <geoff.house@bbc.co.uk>
2026-02-10 08:13:24 +0000

    remove densitron-emu3 module

--- src/modules/densitron-emu3/container/services/device-getstats.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const mongoSingle = require("@core/mongo-single");
-
-module.exports = async (deviceIndex) => {
-    const system = await mongoSingle.get("system");
-    let deviceToReturn = system.devices.find((device) => device.deviceIndex === parseInt(deviceIndex));
-    return deviceToReturn?.meters[0];
-};
--- src/modules/densitron-emu3/container/services/status-get.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const statusCheckCollection = require("@core/status-checkcollection");
-const statusCheckSystem = require("@services/status-checksystem");
-module.exports = async () => {
-    return [].concat(await statusCheckSystem());
-};
