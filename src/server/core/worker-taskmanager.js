86606eb55c46c7eeb4e257006b4a1a149e60b484
Geoff House <geoff.house@bbc.co.uk>
2026-02-11 14:52:11 +0000

    add worker-taskmanager for nested worker tasks

---
+++ src/server/core/worker-taskmanager.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const { ToadScheduler, SimpleIntervalJob, Task } = require("toad-scheduler");
+const logger = require("@core/logger")(module);
+const path = require("path");
+
+let scheduler;
+
+module.exports = ({ tasks, context, baseDir }) => {
+
+    scheduler = new ToadScheduler();
+
+    for (const task of tasks) {
+        const taskPath = path.join(baseDir, "tasks", task.name);
+
+        let handler;
+        try {
+            handler = require(taskPath);
+        } catch (err) {
+            logger.error(`worker-taskmanager: failed to load task file: ${task.name}`);
+            throw err;
+        }
+
+        logger.info(`worker-taskmanager: adding task ${task.name}, interval: ${task.seconds}`);
+        scheduler.addSimpleIntervalJob(
+            new SimpleIntervalJob(
+                { seconds: task.seconds, runImmediately: true },
+                new Task(task.name, async () => {
+                    await task.handler(context);
+                })
+            ),
+            {
+                preventOverrun: true,
+            }
+        );
+    }
+
+    logger.info(`worker-taskmanager: ${tasks.length} task(s) added, scheduler started`);
+};
+
+// keep node alive
+process.stdin.resume();
+
