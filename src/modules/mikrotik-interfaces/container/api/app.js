9f04ae396b0e9443d0b92d4690d8f221f3930f15
Geoff House <geoff.house@bbc.co.uk>
2026-02-24 10:07:23 +0000

    fix logging

--- src/modules/mikrotik-interfaces/container/api/app.js
+++ src/modules/mikrotik-interfaces/container/api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const createError = require("http-errors");
 const workerStore = require("@core/worker-store");
+const logger = require("@core/logger")(module);
 
 // load routes
 const statusRouter = require("@routes/status");
@@ -11,8 +12,8 @@ const validationRouter = require("@routes/validate");
 
 const heapInfo = require("@core/heap-info");
 
-//Print the heap size
-heapInfo(console);
+// print the heap size
+heapInfo(logger);
 
 let app = express();
 
@@ -32,5 +33,23 @@ app.use("/api/config", configRouter);
 app.use("/api/interface", interfaceRouter);
 app.use("/api/validate", validationRouter);
 app.use("*", defaultRouter);
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    const errorLocation = err.stack ? err.stack.split('\n')[1].trim() : "Unknown location";
+
+    logger.error(`ERROR: ${message} | ${errorLocation}`);
+
+    res.status(statusCode).json({
+        status: "error",
+        message: message
+    });
+});
 
 module.exports = app;
