0e681af74c214071b2c150e4e83ca90bd88de6ab
Geoff House <geoff.house@bbc.co.uk>
2026-03-06 13:38:48 +0000

    [bug-app] add ability to delay tasks in worker manager

--- src/server/core/worker-taskmanager.js
+++ src/server/core/worker-taskmanager.js
@@ -21,23 +21,31 @@ module.exports = ({ tasks, context, baseDir }) => {
             throw err;
         }
 
-        logger.info(`worker-taskmanager: adding task ${task.name}, interval: ${task.seconds}`);
-        scheduler.addSimpleIntervalJob(
-            new SimpleIntervalJob(
-                { seconds: task.seconds, runImmediately: true },
-                new Task(task.name, async () => {
-                    await handler(context);
-                })
-            ),
-            {
-                preventOverrun: true,
-            }
-        );
+        const addJob = () => {
+            logger.info(`worker-taskmanager: adding task ${task.name}, interval: ${task.seconds}s`);
+            scheduler.addSimpleIntervalJob(
+                new SimpleIntervalJob(
+                    { seconds: task.seconds, runImmediately: true },
+                    new Task(task.name, async () => {
+                        await handler(context);
+                    })
+                ),
+                {
+                    preventOverrun: true,
+                }
+            );
+        };
+
+        if (task.delay && task.delay > 0) {
+            logger.info(`worker-taskmanager: delaying start of task ${task.name} by ${task.delay}s`);
+            setTimeout(addJob, task.delay * 1000); // convert seconds to ms
+        } else {
+            addJob();
+        }
     }
 
-    logger.info(`worker-taskmanager: ${tasks.length} task(s) added, scheduler started`);
+    logger.info(`worker-taskmanager: ${tasks.length} task(s) scheduled, scheduler started`);
 };
 
 // keep node alive
 process.stdin.resume();
-
