e15622721b7c76b23ec7722be21e6d460083b183
Geoff House <geoff.house@bbc.co.uk>
2026-06-17 17:51:10 +0100

    [mikrotik-sdwan] don't restart worker on wanaddress failure

--- src/modules/mikrotik-sdwan/container/workers/worker-wanaddress.js
+++ src/modules/mikrotik-sdwan/container/workers/worker-wanaddress.js
@@ -37,8 +37,7 @@ const main = async () => {
             timeout: 10,
             keepalive: true,
             onDisconnect: (err) => {
-                logger.error(err.message || err);
-                process.exit(1);
+                logger.warning(err.message || err);
             }
         });
 
