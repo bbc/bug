e82848bc4ab58d61801702dee165c73a5456998f
Geoff House <geoff.house@bbc.co.uk>
2026-07-14 19:12:25 +0100

    [codec-apipoller] don't alert on stale data

--- src/modules/codec-apipoller/container/services/status-get.js
+++ src/modules/codec-apipoller/container/services/status-get.js
@@ -8,13 +8,6 @@ module.exports = async () => {
     try {
         return [].concat(
             await statusGetDefault(),
-            await statusCheckMongoSingle({
-                collectionName: "codecs",
-                message: ["There is no recent codec data for this service.", "Check your settings."],
-                itemType: "critical",
-                timeoutSeconds: 60,
-                flags: ["restartPanel", "configurePanel"],
-            })
         );
     } catch (err) {
         logger.error(err.stack || err.message);
